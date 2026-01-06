f652abed05c1766c04d04fce8cdf3542ad6285ac
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-05 23:31:01 -0500

    feat: created public, compute private, and serverless_private subnets; also renamed the data private subnet resource

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -7,8 +7,32 @@ resource "aws_vpc" "main" {
   }
 }
 
-# CREATE PRIVATE SUBNETS
-resource "aws_subnet" "data_private_subnet" {
+# CREATE SUBNETS
+## PUBLIC SUBNETS
+resource "aws_subnet" "public" {
+  for_each = var.public_subnet_cidrs
+
+  vpc_id = aws_vpc.main.id
+  cidr_block = each.value
+  availability_zone = each.key
+}
+
+## COMPUTE PRIVATE SUBNETS
+resource "aws_subnet" "compute_private" {
+  for_each = var.compute_private_subnet_cidrs
+
+  vpc_id = aws_vpc.main.id
+  cidr_block = each.value
+  availability_zone = each.key
+
+  tags = {
+    Name = "Compute-Private-${each.key}"
+    Terraform = "true"
+  }
+}
+
+## DATA PRIVATE SUBNETS
+resource "aws_subnet" "data_private" {
     for_each = var.data_private_subnet_cidrs
 
     vpc_id = aws_vpc.main.id
@@ -19,4 +43,18 @@ resource "aws_subnet" "data_private_subnet" {
       Name = "Data-Private-${each.key}"
       Terraform = "true"
     }
+}
+
+## SERVERLESS PRIVATE SUBNETS
+resource "aws_subnet" "serverless_private" {
+  for_each = var.serverless_private_subnet_cidrs
+  
+  vpc_id = aws_vpc.main.id
+  cidr_block = each.value
+  availability_zone = each.key
+
+  tags = {
+    Name = "Serverless-Private-${each.key}"
+    Terraform = "true"
+  }
 }
