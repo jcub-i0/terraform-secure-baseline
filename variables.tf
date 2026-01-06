8863441bdfa71a65923d691ddbe6baee9d36937d
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-05 20:16:17 -0500

    Defined main_vpc_cidr and data_private_subnet_cidrs variables

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "main_vpc_cidr" {
+  description = "CIDR block for the primary VPC"
+  default     = "10.0.0.0/16"
+  type        = string
+}
+
+variable "data_private_subnet_cidrs" {
+  description = "Map of AZ -> CIDR for Data Private Subnets"
+  type        = map(string)
+  
+  default = {
+    "us-east-1a" = "10.0.10.0/24"
+    "us-east-1b" = "10.0.12.0/24"
+  }
+}
