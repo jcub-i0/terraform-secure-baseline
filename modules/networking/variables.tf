50b1acbe6377b0b69ee4ccc3cd7447965590fdbe
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-05 23:29:27 -0500

    feat: created public_subnet_cidrs, compute_private_subnet_cidrs, and serverless_private_subnet_cidrs variables in the network module

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -2,6 +2,18 @@ variable "main_vpc_cidr" {
   type = string
 }
 
+variable "public_subnet_cidrs" {
+  type = map(string)
+}
+
+variable "compute_private_subnet_cidrs" {
+  type = map(string)
+}
+
 variable "data_private_subnet_cidrs" {
+  type = map(string)
+}
+
+variable "serverless_private_subnet_cidrs" {
   type = map(string)
 }
