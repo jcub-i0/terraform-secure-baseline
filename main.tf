bff5ae8dacc18f5c19dc96d2fde47070b350afe5
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-05 20:14:39 -0500

    Defined 'networking' module and its 'main_vpc_cidr' and 'data_private_subnet_cidrs' variables

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
 module "networking" {
-  source = "./modules/networking"
+  source                   = "./modules/networking"
+  
+  main_vpc_cidr            = var.main_vpc_cidr
+  data_private_subnet_cidrs = var.data_private_subnet_cidrs
 }
