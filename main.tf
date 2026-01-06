54ce7a7dc7c804daed521a169d99df7a6ab6b2ff
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-05 20:17:51 -0500

    Ran a terraform fmt

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "networking" {
-  source                   = "./modules/networking"
-  
-  main_vpc_cidr            = var.main_vpc_cidr
+  source = "./modules/networking"
+
+  main_vpc_cidr             = var.main_vpc_cidr
   data_private_subnet_cidrs = var.data_private_subnet_cidrs
 }
--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "main_vpc_cidr" {
 variable "data_private_subnet_cidrs" {
   description = "Map of AZ -> CIDR for Data Private Subnets"
   type        = map(string)
-  
+
   default = {
     "us-east-1a" = "10.0.10.0/24"
     "us-east-1b" = "10.0.12.0/24"
