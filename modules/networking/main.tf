6b889de6f154fdacde832b059843089d383f2cf0
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-05 20:15:32 -0500

    set aws_vpc.main.cidr_bblock to var.main_vpc_cidr

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,7 +1,22 @@
+# CREATE MAIN VPC
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.main_vpc_cidr
   tags = {
     Name = "Main-TF-Secure-Baseline"
     Terraform = "true"
   }
+}
+
+# CREATE PRIVATE SUBNETS
+resource "aws_subnet" "data_private_subnet" {
+    for_each = var.data_private_subnet_cidrs
+
+    vpc_id = aws_vpc.main.id
+    cidr_block = each.value
+    availability_zone = each.key
+
+    tags = {
+      Name = "Data-Private-${each.key}"
+      Terraform = "true"
+    }
 }
