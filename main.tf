f1f80b1fe6fdc8096bb5f90c3afc87d0cc26a01a
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-05 23:31:34 -0500

    feat: defined variables in main.tf for the networking module

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 module "networking" {
   source = "./modules/networking"
 
-  main_vpc_cidr             = var.main_vpc_cidr
-  data_private_subnet_cidrs = var.data_private_subnet_cidrs
+  main_vpc_cidr                   = var.main_vpc_cidr
+  public_subnet_cidrs             = var.public_subnet_cidrs
+  compute_private_subnet_cidrs    = var.compute_private_subnet_cidrs
+  data_private_subnet_cidrs       = var.data_private_subnet_cidrs
+  serverless_private_subnet_cidrs = var.serverless_private_subnet_cidrs
 }
