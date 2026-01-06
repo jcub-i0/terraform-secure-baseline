487efc061fb13a22b94a0a54238b982a389f19c7
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-05 23:14:27 -0500

    feat: Created compute_private_subnet_cidrs and serverless_private_subnet_cidrs variables

--- variables.tf
+++ variables.tf
@@ -4,6 +4,16 @@ variable "main_vpc_cidr" {
   type        = string
 }
 
+variable "compute_private_subnet_cidrs" {
+  description = "Map of AZ -> CIDR for Compute Private Subnets"
+  type = map(string)
+
+  default = {
+    "us-east-1a" = "10.0.16.0/24"
+    "us-east-1b" = "10.0.17.0/24"
+  }
+}
+
 variable "data_private_subnet_cidrs" {
   description = "Map of AZ -> CIDR for Data Private Subnets"
   type        = map(string)
@@ -11,5 +21,15 @@ variable "data_private_subnet_cidrs" {
   default = {
     "us-east-1a" = "10.0.32.0/24"
     "us-east-1b" = "10.0.33.0/24"
+  }
+}
+
+variable "serverless_private_subnet_cidrs" {
+  description = "Map of AZ -> CIDR for Serverless Private Subnets"
+  type = map(string)
+
+  default = {
+    "us-east-1a" = "10.0.48.0/24"
+    "us-east-1b" = "10.0.49.0/24"
   }
 }
