6fcf39f68b5496f62d8425f23b39d5b084399e9c
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-06 22:53:23 -0500

    feat: created public route table association with public subnet

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -108,3 +108,8 @@ resource "aws_route_table" "public" {
     Terraform = "true"
   }
 }
+
+resource "aws_route_table_association" "public" {
+  route_table_id = aws_route_table.public.id
+  subnet_id = aws_subnet.public.id
+}
