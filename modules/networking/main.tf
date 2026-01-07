7e18ce6a51d535fc007e059cab885355c7654fbe
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-06 22:41:47 -0500

    feat: created public route table

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -92,3 +92,14 @@ resource "aws_nat_gateway" "natgw" {
     Terraform = "true"
   }
 }
+
+# CREATE AND ASSOCIATE ROUTE TABLES
+## PUBLIC ROUTE TABLE
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
