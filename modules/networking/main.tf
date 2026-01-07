97716c47360f62319c39e195a21f38558524ec60
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-06 22:43:52 -0500

    fix: added standardized tags to public route table

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -102,4 +102,9 @@ resource "aws_route_table" "public" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
+
+  tags = {
+    Name = "Public-Route-Table"
+    Terraform = "true"
+  }
 }
