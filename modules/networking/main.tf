9b84e170833af366e4f32ac785dc10a5b5f7e895
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-07 19:53:16 -0500

    feat: created private subnets' route table

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -119,3 +119,18 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
   subnet_id      = each.value.id
 }
+
+## COMPUTE PRIVATE ROUTE TABLE
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.natgw.id
+  }
+
+  tags = {
+    Name = "Private-Route-Table"
+    Terraform = "true"
+  }
+}
