53edc9f8b7747c54e84ce0552fa6f2bbfa62da8c
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-07 23:51:01 -0500

    feat: created ec2 instance

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -27,10 +27,20 @@ resource "aws_security_group" "compute" {
 
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners = ["099720109477"]
+  owners      = ["099720109477"]
 
   filter {
-    name = "name"
+    name   = "name"
     values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server*"]
+  }
+}
+
+resource "aws_instance" "ec2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name      = "EC2"
+    Terraform = "true"
   }
 }
