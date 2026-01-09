a3332d73ec84e8f2a4e47c2d71710658284d2474
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-09 16:39:21 -0500

    refactor: removed all variables except 'main_vpc_cidr' and replaced them with 'azs' and 'subnet_cidrs'

--- main.tf
+++ main.tf
@@ -2,10 +2,8 @@ module "networking" {
   source = "./modules/networking"
 
   main_vpc_cidr                   = var.main_vpc_cidr
-  public_subnet_cidrs             = var.public_subnet_cidrs
-  compute_private_subnet_cidrs    = var.compute_private_subnet_cidrs
-  data_private_subnet_cidrs       = var.data_private_subnet_cidrs
-  serverless_private_subnet_cidrs = var.serverless_private_subnet_cidrs
+  azs = var.azs
+  subnet_cidrs = var.subnet_cidrs
 }
 
 module "compute" {
