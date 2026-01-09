ce6045fa82b8ca0200d699f186adec514a811fdb
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-09 18:23:38 -0500

    feat: created ec2_ami_name variable

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -6,3 +6,7 @@ variable "compute_private_subnet_ids" {
   description = "map(string) of Compute Private Subnet IDs (az => subnet.id)"
   type = map(string)
 }
+
+variable "ec2_ami_name" {
+  type = string
+}
