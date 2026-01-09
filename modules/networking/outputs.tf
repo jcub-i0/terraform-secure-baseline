bcbbd98c655978b529491aceb51358f0b7490cd4
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-09 17:49:45 -0500

    chore: replaced outputs that output the subnets as objects with outputs that return the IDs of each subnet

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -2,22 +2,22 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-output "public_subnets" {
-  description = "Public subnets by AZ"
-  value       = aws_subnet.public
+output "public_subnet_ids" {
+  description = "map(string) of Public Subnet IDs by AZ"
+  value = {for az, subnet in aws_subnet.public : az => subnet.id}
 }
 
-output "compute_private_subnets" {
-  description = "Compute Private subnets by AZ"
-  value       = aws_subnet.compute_private
+output "compute_private_subnet_ids" {
+  description = "map(string) of Compute Private Subnet IDs"
+  value = {for az, subnet in aws_subnet.compute_private : az => subnet.id}
 }
 
-output "data_private_subnets" {
-  description = "Data Private subnets by AZ"
-  value       = aws_subnet.data_private
+output "data_private_subnet_ids" {
+  description = "map(string) of Data Private Subnet IDs"
+  value = {for az, subnet in aws_subnet.data_private : az => subnet.id}
 }
 
-output "serverless_private_subnets" {
-  description = "Serverless Private subnets by AZ"
-  value       = aws_subnet.serverless_private
+output "serverless_private_subnet_ids" {
+  description = "map(string) of Serverless Private Subnet IDs"
+  value = {for az, subnet in aws_subnet.serverless_private : az => subnet.id}
 }
