e1eb25730c504e6ab2218860a3d9832e06bef23e
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-10 01:00:54 -0500

    feat: defined 'db_usename' variable for the 'storage' module

--- main.tf
+++ main.tf
@@ -20,4 +20,5 @@ module "storage" {
   db_port = var.db_port
   compute_sg_id = module.compute.compute_sg_id
   data_private_subnet_ids_list = module.networking.data_private_subnet_ids_list
+  db_username = var.db_username
 }
