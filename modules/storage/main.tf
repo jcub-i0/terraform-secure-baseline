aacb87f7e0ca5a8e2371122f10c47abbfae7d6f6
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-10 01:04:27 -0500

    feat: created db subnet group, rds instance, and all the resources required to randomly-generate a 20-digit password WITHOUT persisting it to the state file

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,4 +1,4 @@
-# CREATE DATA SECURITY GROUP AND RDS INSTANCE
+# CREATE DATA SECURITY GROUP, DB Subnet Group, AND RDS INSTANCE
 ## DATA SECURITY GROUP
 resource "aws_security_group" "data" {
   name        = "Data-SG"
@@ -27,4 +27,82 @@ resource "aws_security_group" "data" {
     Name      = "Data-SG"
     Terraform = "true"
   }
+}
+
+## DB SUBNET GROUP
+resource "aws_db_subnet_group" "data" {
+  name = "data-db-subnet-group"
+  subnet_ids = var.data_private_subnet_ids_list
+
+  tags = {
+    Name = "Data-DB-Subnet-Group"
+    Terraform = "true"
+  }
+}
+
+## RDS Instance
+resource "aws_db_instance" "main" {
+  identifier = "saas-data-db"
+
+  engine = "postgres"
+  engine_version = "16.6"
+  instance_class = "db.t4g.medium"
+
+  allocated_storage = 50
+  max_allocated_storage = 200
+  storage_type = "gp3"
+  storage_encrypted = true
+
+  db_subnet_group_name = aws_db_subnet_group.data.name
+  vpc_security_group_ids = [aws_security_group.data.id]
+
+  multi_az = true
+  publicly_accessible = false
+
+  db_name = "appdb"
+  username = var.db_username
+  password = jsondecode(data.aws_secretsmanager_secret_version.rds_master.secret_string)["password"]
+
+  deletion_protection = false # CHANGE THIS TO 'TRUE' FOR A PRODUCTION ENVIRONMENT
+  skip_final_snapshot = false
+  backup_retention_period = 14
+  backup_window = "03:00-04:00"
+  maintenance_window = "sun:05:00-sun:06:00"
+
+  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
+
+  performance_insights_enabled = true
+  # monitoring_interval = 60 # IAM ROLE IS REQUIRED TO DO THIS
+
+  auto_minor_version_upgrade = true
+
+  tags = {
+    Name = "SaaS-RDS"
+    Terraform = "true"
+  }
+}
+
+# RDS SECRET GENERATION/HANDLING, WHERE THE SECRET IS NEVER PERSISTED TO THE STATE
+## Create a secret in AWS Secrets Manager
+resource "aws_secretsmanager_secret" "rds_master" {
+  name = "rds-secret"
+}
+
+## Generate random secret
+ephemeral "aws_secretsmanager_random_password" "rds_master" {
+  password_length = 20
+}
+
+## Store generated secret inside the secret in AWS Secrets Manager
+resource "aws_secretsmanager_secret_version" "rds_master" {
+  secret_id = aws_secretsmanager_secret.rds_master.id
+  secret_string_wo = jsonencode({
+    password = ephemeral.aws_secretsmanager_random_password.rds_master.random_password
+  })
+  secret_string_wo_version = 1
+}
+
+## Access the generated secret inside the AWS Secrets Manager secret
+data "aws_secretsmanager_secret_version" "rds_master" {
+  secret_id = aws_secretsmanager_secret.rds_master.id
 }
