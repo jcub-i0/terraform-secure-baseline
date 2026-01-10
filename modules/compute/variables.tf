41252fcc916193a3d00472f3067e0de23f9b8fe4
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-10 00:51:34 -0500

    fix: renamed 'compute_private_subnet_ids' to 'compute_private_subnet_ids_map'

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -2,7 +2,7 @@ variable "vpc_id" {
   type = string
 }
 
-variable "compute_private_subnet_ids" {
+variable "compute_private_subnet_ids_map" {
   description = "map(string) of Compute Private Subnet IDs (az => subnet.id)"
   type = map(string)
 }
