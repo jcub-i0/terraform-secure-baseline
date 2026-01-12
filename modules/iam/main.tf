8f648b42f15060de63bcd2c7bfa39c7b4844e8a0
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-12 13:03:49 -0500

    feat: created 'ec2_role' IAM Role

---
+++ modules/iam/main.tf
@@ -0,0 +1,16 @@
+# EC2 Roles and Policies
+## EC2 Role
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_compute_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazon.com"
+      }
+    }]
+  })
+}
