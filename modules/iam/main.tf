acf150f59cbf630fefad85b05d146804ad27e655
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-12 13:24:21 -0500

    feat: created 'cloudwatch' iam policy and attached it to ec2_role iam role

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -20,3 +20,9 @@ resource "aws_iam_role_policy_attachment" "ssm" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
+
+## Allow EC2 to push logs and metrics to CloudWatch
+resource "aws_iam_role_policy_attachment" "cloudwatch" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
