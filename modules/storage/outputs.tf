4c63f2fbbf5384f0b59fcb0f5e84037794e9f579
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-12 21:19:58 -0500

    feat: created outputs for the .name, .arn, and .id attributes of the Centralized Logs S3 bucket

---
+++ modules/storage/outputs.tf
@@ -0,0 +1,14 @@
+output "centralized_logs_bucket_name" {
+  description = "The 'bucket' attribute of the Centralized Logs S3 bucket"
+  value       = aws_s3_bucket.centralized_logs.bucket
+}
+
+output "centralized_logs_bucket_arn" {
+  description = "The ARN of the Centralized Logs S3 bucket"
+  value       = aws_s3_bucket.centralized_logs.arn
+}
+
+output "centralized_logs_bucket_id" {
+  description = "The ID of the Centralized Logs S3 bucket"
+  value       = aws_s3_bucket.centralized_logs.id
+}
