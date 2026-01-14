5ad91675c675f92370deefb3c8d3d7b46921adc4
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-13 23:49:19 -0500

    style: ran a terraform fmt for main.tf in logging module

--- modules/logging/main.tf
+++ modules/logging/main.tf
@@ -1,26 +1,26 @@
 # CLOUDWATCH
 ## CLOUDWATCH LOG GROUP
 resource "aws_cloudwatch_log_group" "cloudtrail" {
-  name = "/aws/cloudtrail/tf-secure-baseline"
+  name              = "/aws/cloudtrail/tf-secure-baseline"
   retention_in_days = 90
-  kms_key_id = var.logs_kms_key_arn
+  kms_key_id        = var.logs_kms_key_arn
 }
 
 # CLOUDTRAIL
 resource "aws_cloudtrail" "cloudtrail" {
-  name = "CloudTrail"
-  s3_bucket_name = var.centralized_logs_bucket_id
-  kms_key_id = var.logs_kms_key_arn
-  is_multi_region_trail = true
-  enable_logging = true
-  enable_log_file_validation = true
+  name                          = "CloudTrail"
+  s3_bucket_name                = var.centralized_logs_bucket_id
+  kms_key_id                    = var.logs_kms_key_arn
+  is_multi_region_trail         = true
+  enable_logging                = true
+  enable_log_file_validation    = true
   include_global_service_events = true
 
   cloud_watch_logs_group_arn = aws_cloudwatch_log_group.cloudtrail.arn
-  cloud_watch_logs_role_arn = var.cloudtrail_role_arn
+  cloud_watch_logs_role_arn  = var.cloudtrail_role_arn
 
   event_selector {
-    read_write_type = "All"
+    read_write_type           = "All"
     include_management_events = true
   }
 
