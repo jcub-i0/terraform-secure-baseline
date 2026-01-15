1774d8f7fb1a2e0037accc6f39394bef41010db8
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-14 19:10:45 -0500

    fix: deleted cloudtrail arn variable - not being used (yet, at least)

--- modules/logging/outputs.tf
+++ modules/logging/outputs.tf
@@ -1,8 +1,3 @@
 output "cloudtrail_log_group_arn" {
   value = aws_cloudwatch_log_group.cloudtrail.arn
 }
-
-output "cloudtrail_arn" {
-    description = "The ARN of the CloudTrail resource itself"
-  value = aws_cloudtrail.cloudtrail.arn
-}
