05135514765eed43995ceaabb03e22f5e01ac400
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-14 19:08:21 -0500

    feat: created 'cloudtrail_arn' resource

--- modules/logging/outputs.tf
+++ modules/logging/outputs.tf
@@ -1,3 +1,8 @@
 output "cloudtrail_log_group_arn" {
   value = aws_cloudwatch_log_group.cloudtrail.arn
 }
+
+output "cloudtrail_arn" {
+    description = "The ARN of the CloudTrail resource itself"
+  value = aws_cloudtrail.cloudtrail.arn
+}
