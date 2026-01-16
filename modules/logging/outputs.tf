e1f61880b840700547123b45e99e149f95ddbbab
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-16 17:23:41 -0500

    feat: created cloudtrail_logs_group_name variable

--- modules/logging/outputs.tf
+++ modules/logging/outputs.tf
@@ -1,3 +1,7 @@
 output "cloudtrail_log_group_arn" {
   value = aws_cloudwatch_log_group.cloudtrail.arn
 }
+
+output "cloudtrail_logs_group_name" {
+  value = aws_cloudwatch_log_group.cloudtrail.name
+}
