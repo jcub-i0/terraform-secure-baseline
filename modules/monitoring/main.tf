117459c8ff7d7e5a3d71f724a49fdb4b8aaf9b67
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-16 16:13:29 -0500

    refactor: renamed SNS topics from 'config' -> 'compliance' and 'cloudtrail' -> 'security'

--- modules/monitoring/main.tf
+++ modules/monitoring/main.tf
@@ -1,8 +1,9 @@
 # SNS
 ## SNS RESOURCES FOR CONFIG
 ### CONFIG SNS TOPIC
-resource "aws_sns_topic" "config" {
-  name = "config-notifications"
+resource "aws_sns_topic" "compliance" {
+  name = "compliance-notifications"
+  kms_master_key_id = var.logs_kms_key_arn
 
   tags = {
     Name = "ConfigNotifications"
@@ -11,8 +12,8 @@ resource "aws_sns_topic" "config" {
 }
 
 ### CONFIG SNS TOPIC POLICY
-resource "aws_sns_topic_policy" "config" {
-  arn = aws_sns_topic.config.arn
+resource "aws_sns_topic_policy" "compliance" {
+  arn = aws_sns_topic.compliance.arn
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
@@ -21,7 +22,35 @@ resource "aws_sns_topic_policy" "config" {
             Service = "config.amazonaws.com"
         }
         Action = "sns:Publish"
-        Resource = aws_sns_topic.config.arn
+        Resource = aws_sns_topic.compliance.arn
+    }]
+  })
+}
+
+## SNS RESOURCES FOR CLOUDTRAIL
+### CLOUDTRAIL SNS TOPIC
+resource "aws_sns_topic" "security" {
+  name = "security-notifications"
+
+  tags = {
+    Name = "CloudtrailNotifications"
+    Terraform = "true"
+  }
+}
+
+### CLOUDTRAIL SNS TOPIC POLICY
+resource "aws_sns_topic_policy" "security" {
+  arn = aws_sns_topic.security.arn
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+        Effect = "Allow"
+        Principal = {
+            "Service" = "cloudtrail.amazonaws.com"
+        }
+        Action = "sns:Publish"
+        Resource = aws_sns_topic.security.arn
     }]
   })
 }
