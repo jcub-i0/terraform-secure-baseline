f8264d4b1e89c577e54089fb6f8800369f627c8b
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-17 13:31:16 -0500

    feat: created event rule to trigger upon ec2 high/critical alerts

---
+++ modules/automation/main.tf
@@ -0,0 +1,22 @@
+# EVENTBRIDGE RESOURCES
+## EVENT RULE TO TRIGGER UPON HIGH/CRITICAL SECURITY HUB EC2 FINDINGS
+resource "aws_cloudwatch_event_rule" "securityhub_ec2_high_critical" {
+  name = "securityhub-ec2-high-critical"
+  description = "High/Critical Security Hub EC2 findings"
+
+  event_pattern = jsonencode({
+    source = ["aws.securityhub"]
+    detail-type = ["Security Hub Findings - Imported"]
+    detail = {
+        severity = {
+            label = ["HIGH", "CRITICAL"]
+        }
+        resources = {
+            type = ["AwsEc2Instance"]
+        }
+        compliance = {
+            status = ["FAILED"]
+        }
+    }
+  })
+}
