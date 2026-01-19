7035fd73f2010a7b72c91cd360d729dbe5f69d58
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-19 17:38:27 -0500

    feat: created SG for the EC2 Isolation Lambda func

--- modules/automation/main.tf
+++ modules/automation/main.tf
@@ -23,3 +23,24 @@ resource "aws_cloudwatch_event_rule" "securityhub_ec2_high_critical" {
     }
   })
 }
+
+# EC2 ISOLATION LAMBDA RESOURCES
+## EC2 ISOLATION SECURITY GROUP
+resource "aws_security_group" "Lambda_EC2_Isolation-SG" {
+  name = "Lambda-EC2-Isolation-SG"
+  description = "Security Group for the EC2 Isolation Lambda function"
+  vpc_id = var.vpc_id
+
+  egress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "AWS API Access"
+  }
+
+  tags = {
+    Name = "Lambda-EC2-Isolation-SG"
+    Terraform = "true"
+  }
+}
