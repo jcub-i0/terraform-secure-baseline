5393201955df8fe308ad9d9af889403d3eac6e0d
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-19 19:52:35 -0500

    chore: added and deleted some comments

--- modules/automation/main.tf
+++ modules/automation/main.tf
@@ -1,4 +1,4 @@
-# EVENTBRIDGE RESOURCES
+# EC2 ISOLATION LAMBDA RESOURCES
 ## EVENT RULE TO TRIGGER UPON HIGH/CRITICAL SECURITY HUB EC2 FINDINGS
 resource "aws_cloudwatch_event_rule" "securityhub_ec2_high_critical" {
   name        = "securityhub-ec2-high-critical"
@@ -24,7 +24,6 @@ resource "aws_cloudwatch_event_rule" "securityhub_ec2_high_critical" {
   })
 }
 
-# EC2 ISOLATION LAMBDA RESOURCES
 ## EC2 ISOLATION SECURITY GROUP
 resource "aws_security_group" "Lambda_EC2_Isolation-SG" {
   name        = "Lambda-EC2-Isolation-SG"
