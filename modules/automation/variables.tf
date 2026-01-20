c7047b71cda5fb158e7e2fca1056a112c1a30d21
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-19 20:20:42 -0500

    feat: created several variables within the 'automation' module

--- modules/automation/variables.tf
+++ modules/automation/variables.tf
@@ -1,3 +1,15 @@
 variable "vpc_id" {
   type = string
 }
+
+variable "lambda_ec2_isolation_role_arn" {
+  type = string
+}
+
+variable "serverless_private_subnet_ids" {
+  type = list(string)
+}
+
+variable "quarantine_sg_id" {
+  type = string
+}
