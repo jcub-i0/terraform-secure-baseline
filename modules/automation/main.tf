30ca466d9c8cf1979674ef9eb7d0fc9334ca1bc0
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-25 00:21:08 -0500

    fix: fixed ec2 isolation event rule's pattern; it's triggering as intended now

--- modules/automation/main.tf
+++ modules/automation/main.tf
@@ -42,17 +42,19 @@ resource "aws_cloudwatch_event_rule" "securityhub_ec2_high_critical" {
   description = "High/Critical Security Hub EC2 findings"
 
   event_pattern = jsonencode({
-    source      = ["aws.securityhub"]
-    detail-type = ["Security Hub Findings - Imported"]
+    source      = ["aws.securityhub"],
+    detail-type = ["Security Hub Findings - Imported"],
     detail = {
-      severity = {
-        label = ["HIGH", "CRITICAL"]
-      }
-      resources = {
-        type = ["AwsEc2Instance"]
-      }
-      workflow = {
-        status = ["NEW"]
+        findings = {
+            Severity = {
+                Label = ["HIGH", "CRITICAL"]
+            },
+            Resources = {
+                Type = ["AwsEc2Instance"]
+            },
+            Workflow = {
+                Status = ["NEW"]
+            }
       }
     }
   })
