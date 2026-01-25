d5ccbb674a373b10cd8cc66dc4b4e8d9986e348d
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-24 19:04:10 -0500

    chore: updated 'Name' tag for EC2 instnaces to use 'EC2-{AZ}' format

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -76,7 +76,7 @@ resource "aws_instance" "ec2" {
   }
 
   tags = {
-    Name             = "EC2"
+    Name             = "EC2-${each.key}"
     Terraform        = "true"
     Purpose          = "Receives input from users or other services, transforms it, validates it, and/or aggregates it"
     IsolationAllowed = "true"
