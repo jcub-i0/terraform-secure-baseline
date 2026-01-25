5be64c92c16e72953655c912e66d57fcc94c9ea0
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-25 13:44:33 -0500

    chore: created lambda_ec2_rollback_role_arn variable

--- modules/automation/variables.tf
+++ modules/automation/variables.tf
@@ -6,6 +6,10 @@ variable "lambda_ec2_isolation_role_arn" {
   type = string
 }
 
+variable "lambda_ec2_rollback_role_arn" {
+  type = string
+}
+
 variable "serverless_private_subnet_ids" {
   type = list(string)
 }
