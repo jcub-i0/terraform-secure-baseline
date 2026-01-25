6671be7308dae2f0a4c2b9cd9e0a28b8b5c280f0
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-24 20:42:58 -0500

    chore: chore: created variables for the ebs kms key arn and it's alias' arn

--- modules/security/outputs.tf
+++ modules/security/outputs.tf
@@ -2,3 +2,13 @@ output "logs_kms_key_arn" {
   description = "The ARN of the KMS CMK"
   value       = aws_kms_key.logs.arn
 }
+
+output "ebs_kms_key_arn" {
+  description = "The ARN of the EBS KMS CMK"
+  value = aws_kms_key.ebs.arn
+}
+
+output "ebs_kms_alias_arn" {
+  description = "The EBS KMS CMK alias"
+  value = aws_kms_alias.ebs.arn
+}
