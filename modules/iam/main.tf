ca115d73858f6b4f98c4720527a64b20dba6d875
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-25 21:07:44 -0500

    chore: removed config's role and policy; replaced it with AWS' service-linked role for config

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -67,27 +67,9 @@ resource "aws_iam_role_policy" "cloudtrail" {
   })
 }
 
-# CONFIG
-## CONFIG ROLE
-resource "aws_iam_role" "config" {
-  name = "aws-config-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "config.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-## CONFIG ROLE POLICY
-resource "aws_iam_role_policy_attachment" "config" {
-  role       = aws_iam_role.config.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWS_ConfigRole"
+# CONFIG SERVICE-LINKED ROLE
+resource "aws_iam_service_linked_role" "config" {
+  aws_service_name = "config.amazonaws.com"
 }
 
 # LAMBDA ROLES
