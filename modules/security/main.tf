8384b4020e0db86f4c7c4cb9265a2be47b1b420b
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-30 17:48:50 -0500

    feat: created config remediation configuration resource for s3_public_access_block

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -51,6 +51,26 @@ resource "aws_config_configuration_recorder_status" "config" {
 }
 
 # CONFIG REMEDIATIONS
+## S3 PUBLIC ACCESS BLOCK CONFIG REMEDIATION
+resource "aws_config_remediation_configuration" "s3_public_access_block" {
+  config_rule_name = aws_config_config_rule.s3_public_access_block.name
+  resource_type = "AWS::S3::Bucket"
+  target_type      = "SSM_DOCUMENT"
+  target_id        = "AWSConfigRemediation-ConfigureS3BucketPublicAccessBlock"
+  automatic        = true
+  maximum_automatic_attempts = 3
+  retry_attempt_seconds      = 60
+
+  parameter {
+    name = "AutomationAssumeRole"
+    static_value = var.config_remediation_role_arn
+  }
+
+  parameter {
+    name = "BucketName"
+    resource_value = "RESOURCE_ID"
+  }
+}
 
 resource "aws_config_config_rule" "s3_public_access_block" {
   name = "s3-bucket-public-read-block"
