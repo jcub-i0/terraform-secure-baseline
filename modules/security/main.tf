91e42ec68cd604600fa361409918620ec8e77237
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-01-30 19:30:03 -0500

    chore: changed order and added comments for s3 public access block config remediation

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -51,6 +51,17 @@ resource "aws_config_configuration_recorder_status" "config" {
 
 # CONFIG REMEDIATIONS
 ## S3 PUBLIC ACCESS BLOCK CONFIG REMEDIATION
+### S3 PUBLIC ACCESS REMEDIATION RULE
+resource "aws_config_config_rule" "s3_public_access_block" {
+  name = "s3-bucket-public-read-block"
+
+  source {
+    owner = "AWS"
+    source_identifier = "S3_BUCKET_LEVEL_PUBLIC_ACCESS_PROHIBITED"
+  }
+}
+
+### REMEDIATION TO AUTOMATICALLY DISABLE S3 PUBLIC READ AND WRITE
 resource "aws_config_remediation_configuration" "s3_public_access_block" {
   config_rule_name = aws_config_config_rule.s3_public_access_block.name
   resource_type = "AWS::S3::Bucket"
@@ -68,15 +79,6 @@ resource "aws_config_remediation_configuration" "s3_public_access_block" {
   parameter {
     name = "BucketName"
     resource_value = "RESOURCE_ID"
-  }
-}
-
-resource "aws_config_config_rule" "s3_public_access_block" {
-  name = "s3-bucket-public-read-block"
-
-  source {
-    owner = "AWS"
-    source_identifier = "S3_BUCKET_LEVEL_PUBLIC_ACCESS_PROHIBITED"
   }
 }
 
