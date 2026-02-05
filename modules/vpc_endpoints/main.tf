483bc68ea4b5db0e8a9aa13b5507384fb1a2cf3e
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-02-04 20:29:05 -0500

    feat: created s3 gateway endpoint

---
+++ modules/vpc_endpoints/main.tf
@@ -0,0 +1,26 @@
+locals {
+  private_subnet_ids = concat(
+    var.compute_subnet_ids_list,
+    var.data_private_subnet_ids_list,
+    var.serverless_private_subnet_ids_list
+  )
+}
+
+data "aws_route_table" "private" {
+  for_each = toset(local.private_subnet_ids)
+  subnet_id = each.value
+}
+
+# GATEWAY ENDPOINTS (S3)
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id = var.vpc_id
+  service_name = "com.amazonaws.${var.primary_region}.s3"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = [for rt in values(data.aws_route_table.private) : rt.id]
+
+  tags = {
+    Name = "S3-Gateway-Endpoint"
+    Terraform = "true"
+  }
+}
