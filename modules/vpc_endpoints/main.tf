e6456100a2eb2f201d8bf14cdba783e5df8d294d
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-02-04 23:17:39 -0500

    chore: created vpc endpoint for sts

--- modules/vpc_endpoints/main.tf
+++ modules/vpc_endpoints/main.tf
@@ -59,3 +59,12 @@ resource "aws_security_group" "interface_endpoints_sg" {
     Terraform = "true"
   }
 }
+
+resource "aws_vpc_endpoint" "sts" {
+  vpc_id = var.vpc_id
+  service_name = "com.amazonaws.${var.primary_region}.sts"
+  vpc_endpoint_type = "Interface"
+  subnet_ids = local.private_subnet_ids
+  security_group_ids = [aws_security_group.interface_endpoints_sg.id]
+  private_dns_enabled = true
+}
