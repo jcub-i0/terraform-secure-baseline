0c90621e9dca21639393afbc8280fb992b3fa5f6
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-02-04 23:16:09 -0500

    feat: created locals variable to return all private subnet cidrs and created security group for all interface VPC endpoints

--- modules/vpc_endpoints/main.tf
+++ modules/vpc_endpoints/main.tf
@@ -4,17 +4,16 @@ locals {
     var.data_private_subnet_ids_list,
     var.serverless_private_subnet_ids_list
   )
+  private_subnet_cidrs = flatten([
+    for subnet, cidr in var.subnet_cidrs :
+    cidr if endswith(subnet, "_private")
+  ])
 }
 
 # GET ROUTE TABLE FOR EACH PRIVATE SUBNET
 data "aws_route_table" "private" {
   for_each = toset(local.private_subnet_ids)
   subnet_id = each.value
-}
-
-# GET ROUTE TABLE FOR EACH PUBLIC SUBNET
-data "aws_route_table" "public" {
-  
 }
 
 # GATEWAY ENDPOINTS (S3, DYNAMODB)
@@ -27,6 +26,36 @@ resource "aws_vpc_endpoint" "s3" {
 
   tags = {
     Name = "S3-Gateway-Endpoint"
+    Terraform = "true"
+  }
+}
+
+# INTERFACE ENDPOINTS
+
+## SECURITY GROUP FOR ALL INTERFACE VPC ENDPOINTS
+resource "aws_security_group" "interface_endpoints_sg" {
+  name = "vpc-endpoints-sg"
+  description = "Security Group for Interface VPC Endpoints (AWS PrivateLink)"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = local.private_subnet_cidrs
+    description = "Allow AWS services to communicate with VPC Endpoints"
+  }
+
+  egress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = local.private_subnet_cidrs
+    description = "Allow VPC Endpoints to communicate with AWS services"
+  }
+
+  tags = {
+    Name = "VPC-Endpoints-SG"
     Terraform = "true"
   }
 }
