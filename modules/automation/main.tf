7d635f3780ab18ce4beab663aa8b6094fc83c3b4
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-02-06 16:44:35 -0500

    chore: edited comment

--- modules/automation/main.tf
+++ modules/automation/main.tf
@@ -120,7 +120,7 @@ resource "aws_lambda_function" "ec2_rollback" {
   memory_size      = 256
   source_code_hash = data.archive_file.lambda_ec2_rollback.output_base64sha256
 
-  # ENABLE X-RAY TRACING1
+  # ENABLE X-RAY TRACING
   tracing_config {
     mode = "Active"
   }
