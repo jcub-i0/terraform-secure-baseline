e23125d612633569aaf82755e8aca6cd3b1fb57c
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-02-06 14:04:16 -0500

    style: ran a terraform fmt

--- modules/vpc_endpoints/main.tf
+++ modules/vpc_endpoints/main.tf
@@ -18,20 +18,20 @@ locals {
 
 # GET ROUTE TABLE FOR EACH PRIVATE SUBNET
 data "aws_route_table" "endpoint" {
-  for_each = local.endpoint_subnets
+  for_each  = local.endpoint_subnets
   subnet_id = each.value
 }
 
 # GATEWAY ENDPOINTS (S3, DYNAMODB)
 resource "aws_vpc_endpoint" "s3" {
-  vpc_id = var.vpc_id
-  service_name = "com.amazonaws.${var.primary_region}.s3"
+  vpc_id            = var.vpc_id
+  service_name      = "com.amazonaws.${var.primary_region}.s3"
   vpc_endpoint_type = "Gateway"
 
   route_table_ids = [for rt in values(data.aws_route_table.endpoint) : rt.id]
 
   tags = {
-    Name = "S3-Gateway-Endpoint"
+    Name      = "S3-Gateway-Endpoint"
     Terraform = "true"
   }
 }
@@ -40,42 +40,42 @@ resource "aws_vpc_endpoint" "s3" {
 
 ## SECURITY GROUP FOR ALL INTERFACE VPC ENDPOINTS
 resource "aws_security_group" "interface_endpoints_sg" {
-  name = "vpc-endpoints-sg"
+  name        = "vpc-endpoints-sg"
   description = "Security Group for Interface VPC Endpoints (AWS PrivateLink)"
-  vpc_id = var.vpc_id
+  vpc_id      = var.vpc_id
 
   ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = local.endpoint_subnet_cidrs
     description = "Allow AWS services to communicate with VPC Endpoints"
   }
 
   egress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = local.endpoint_subnet_cidrs
     description = "Allow VPC Endpoints to communicate with AWS services"
   }
 
   tags = {
-    Name = "VPC-Endpoints-SG"
+    Name      = "VPC-Endpoints-SG"
     Terraform = "true"
   }
 }
 
 resource "aws_vpc_endpoint" "interface" {
-  for_each = toset(local.interface_endpoints)
-  vpc_id = var.vpc_id
-  service_name = "com.amazonaws.${var.primary_region}.${each.key}"
-  vpc_endpoint_type = "Interface"
-  subnet_ids = values(local.endpoint_subnets)
-  security_group_ids = [aws_security_group.interface_endpoints_sg.id]
+  for_each            = toset(local.interface_endpoints)
+  vpc_id              = var.vpc_id
+  service_name        = "com.amazonaws.${var.primary_region}.${each.key}"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = values(local.endpoint_subnets)
+  security_group_ids  = [aws_security_group.interface_endpoints_sg.id]
   private_dns_enabled = true
   tags = {
-    Name = "VPC-Endpoint-${each.key}"
+    Name      = "VPC-Endpoint-${each.key}"
     Terraform = "true"
   }
 }
