01950f8596ffcf08dd5edda03d6b372e004df91c
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-02-12 00:37:44 -0500

    chore: removed rules from compute SG

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -5,22 +5,6 @@ resource "aws_security_group" "compute" {
   description = "Security Group for EC2 compute instances"
   vpc_id      = var.vpc_id
 
-  egress {
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    security_groups = [var.interface_endpoints_sg_id]
-    description     = "Allow outbound HTTPS only to Interface VPC Endpoints"
-  }
-
-  egress {
-    from_port       = var.db_port
-    to_port         = var.db_port
-    protocol        = "tcp"
-    security_groups = [var.data_sg_id]
-    description     = "Allow DB access"
-  }
-
   tags = {
     Name      = "Compute-SG"
     Terraform = "true"
