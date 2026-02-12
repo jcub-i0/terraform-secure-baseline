5b62d533c2bb0ad36eb39770a587b02b4e0ea1c3
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-02-11 23:33:52 -0500

    chore: defined interface_endpoints_sg_id, data_sg_id, and db_port variables in the 'compute' module

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -14,3 +14,15 @@ variable "instance_profile_name" {
 variable "ebs_kms_key_arn" {
   type = string
 }
+
+variable "interface_endpoints_sg_id" {
+  type = string
+}
+
+variable "data_sg_id" {
+  type = string
+}
+
+variable "db_port" {
+  type = string
+}
