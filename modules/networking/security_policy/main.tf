732305b744f9771bd92b11da96d0086ae8908e02
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-02-12 00:45:53 -0500

    chore: created standard files for the 'security_policy' child module

--- modules/networking/security_policy/main.tf
+++ modules/networking/security_policy/main.tf
@@ -2,33 +2,33 @@
 
 ## INTERFACE VPC ENDPOINT SG RULES
 resource "aws_security_group_rule" "endpoints_ingress_from_compute" {
-  type = "ingress"
-  security_group_id = var.interface_endpoints_sg_id
-  from_port = 443
-  to_port = 443
-  protocol = "tcp"
+  type                     = "ingress"
+  security_group_id        = var.interface_endpoints_sg_id
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
   source_security_group_id = var.compute_sg_id
-  description = "Compute to Interface VPC Endpoints over HTTPS"
+  description              = "Compute to Interface VPC Endpoints over HTTPS"
 }
 
 resource "aws_security_group_rule" "endpoints_ingress_from_lambda_isolation" {
-  type = "ingress"
-  security_group_id = var.interface_endpoints_sg_id
-  from_port = 443
-  to_port = 443
-  protocol = "tcp"
+  type                     = "ingress"
+  security_group_id        = var.interface_endpoints_sg_id
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
   source_security_group_id = var.lambda_ec2_isolation_sg_id
-  description = "Lambda EC2 Isolation to Interface VPC Endpoints over HTTPS"
+  description              = "Lambda EC2 Isolation to Interface VPC Endpoints over HTTPS"
 }
 
 resource "aws_security_group_rule" "endpoints_ingress_from_lambda_rollback" {
-  type = "ingress"
-  security_group_id = var.interface_endpoints_sg_id
-  from_port = 443
-  to_port = 443
-  protocol = "tcp"
+  type                     = "ingress"
+  security_group_id        = var.interface_endpoints_sg_id
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
   source_security_group_id = var.lambda_ec2_rollback_sg_id
-  description = "Lambda EC2 Rollback to Interface VPC Endpoints over HTTPS"
+  description              = "Lambda EC2 Rollback to Interface VPC Endpoints over HTTPS"
 }
 
 resource "aws_security_group_rule" "endpoints_egress_any" {
@@ -43,54 +43,54 @@ resource "aws_security_group_rule" "endpoints_egress_any" {
 
 ## COMPUTE SG RULES
 resource "aws_security_group_rule" "compute_egress_to_endpoints" {
-  type = "egress"
-  security_group_id = var.compute_sg_id
-  from_port = 443
-  to_port = 443
-  protocol = "tcp"
+  type                     = "egress"
+  security_group_id        = var.compute_sg_id
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
   source_security_group_id = var.interface_endpoints_sg_id
-  description = "Compute to VPC Endpoints over HTTPS"
+  description              = "Compute to VPC Endpoints over HTTPS"
 }
 
 resource "aws_security_group_rule" "compute_egress_to_db" {
-  type = "egress"
-  security_group_id = var.compute_sg_id
-  from_port = var.db_port
-  to_port = var.db_port
-  protocol = "tcp"
+  type                     = "egress"
+  security_group_id        = var.compute_sg_id
+  from_port                = var.db_port
+  to_port                  = var.db_port
+  protocol                 = "tcp"
   source_security_group_id = var.data_sg_id
-  description = "Compute to DB"
+  description              = "Compute to DB"
 }
 
 ## DATA SG RULES
 resource "aws_security_group_rule" "db_ingress_from_compute" {
-  type = "ingress"
-  security_group_id = var.data_sg_id
-  from_port = var.db_port
-  to_port = var.db_port
-  protocol = "tcp"
+  type                     = "ingress"
+  security_group_id        = var.data_sg_id
+  from_port                = var.db_port
+  to_port                  = var.db_port
+  protocol                 = "tcp"
   source_security_group_id = var.data_sg_id
-  description = "Compute to DB"
+  description              = "Compute to DB"
 }
 
 ## LAMBDA EC2 ISOLATION SG RULES
 resource "aws_security_group_rule" "lambda_isolation_egress_to_endpoints" {
-  type = "egress"
-  security_group_id = var.lambda_ec2_isolation_sg_id
-  from_port = 443
-  to_port = 443
-  protocol = "tcp"
+  type                     = "egress"
+  security_group_id        = var.lambda_ec2_isolation_sg_id
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
   source_security_group_id = var.lambda_ec2_isolation_sg_id
-  description = "Lambda EC2 Isolation to VPC Endpoints over HTTPS"
+  description              = "Lambda EC2 Isolation to VPC Endpoints over HTTPS"
 }
 
 ## LAMBDA EC2 ROLLBACK SG RULES
 resource "aws_security_group_rule" "lambda_rollback_egress_to_endpoints" {
-  type = "egress"
-  security_group_id = var.lambda_ec2_rollback_sg_id
-  from_port = 443
-  to_port = 443
-  protocol = "tcp"
+  type                     = "egress"
+  security_group_id        = var.lambda_ec2_rollback_sg_id
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
   source_security_group_id = var.lambda_ec2_rollback_sg_id
-  description = "Lambda EC2 Rollback to VPC Endpoints over HTTPS"
+  description              = "Lambda EC2 Rollback to VPC Endpoints over HTTPS"
 }
---
+++ modules/networking/security_policy/variables.tf
@@ -0,0 +1,23 @@
+variable "compute_sg_id" {
+  type = string
+}
+
+variable "data_sg_id" {
+  type = string
+}
+
+variable "lambda_ec2_isolation_sg_id" {
+  type = string
+}
+
+variable "lambda_ec2_rollback_sg_id" {
+  type = string
+}
+
+variable "interface_endpoints_sg_id" {
+  type = string
+}
+
+variable "db_port" {
+  type = string
+}
