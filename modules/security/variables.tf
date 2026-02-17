472dea4da70e6c6a052e3006396e38c757d5d908
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-02-17 00:21:16 -0500

    chore: defined 'ec2_baseline' rule in 'enable_rules' variable

--- modules/security/variables.tf
+++ modules/security/variables.tf
@@ -46,6 +46,7 @@ variable "enable_rules" {
     ebs_baseline        = bool
     sg_baseline         = bool
     iam_baseline        = bool
+    ec2_baseline = bool
   })
 
   default = {
@@ -55,5 +56,6 @@ variable "enable_rules" {
     ebs_baseline        = true
     sg_baseline         = true
     iam_baseline        = false
+    ec2_baseline = true
   }
 }
