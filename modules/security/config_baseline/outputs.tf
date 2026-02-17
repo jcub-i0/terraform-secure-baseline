d51dacd8754b0647195b71a5ff22f3ea8adde46b
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-02-17 00:47:25 -0500

    style: ran a terraform fmt

--- modules/security/config_baseline/outputs.tf
+++ modules/security/config_baseline/outputs.tf
@@ -5,7 +5,7 @@ output "managed_config_rule_names" {
 
 output "managed_config_rule_arns" {
   description = "ARNs of AWS-managed Config rules created by this baseline pack"
-  value = [for r in aws_aws_config_config_rule.managed : r.arn]
+  value       = [for r in aws_aws_config_config_rule.managed : r.arn]
 }
 
 output "s3_public_access_remediation_rule_name" {
@@ -15,10 +15,10 @@ output "s3_public_access_remediation_rule_name" {
 
 output "config_recorder_name" {
   description = "AWS Config configuration recorder name"
-  value = aws_config_configuration_recorder.config.name
+  value       = aws_config_configuration_recorder.config.name
 }
 
 output "enabled_rule_families" {
   description = "Rule families enabled in this baseline"
-  value = local.enabled_families
+  value       = local.enabled_families
 }
