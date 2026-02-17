8342c5949acc16a73cfbb16124e531d7fadc67fa
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-02-17 00:42:14 -0500

    chore: created output for config_recorder_name

--- modules/security/config_baseline/outputs.tf
+++ modules/security/config_baseline/outputs.tf
@@ -12,3 +12,8 @@ output "s3_public_access_remediation_rule_name" {
   description = "Config rule name for S3 public access prohibition (auto-remediation target)."
   value       = aws_config_config_rule.s3_public_access_block.name
 }
+
+output "config_recorder_name" {
+  description = "AWS Config configuration recorder name"
+  value = aws_config_configuration_recorder.config.name
+}
