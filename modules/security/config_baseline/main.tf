d6949f82e717d841d711480a0475581bb946f9bf
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-02-16 23:16:42 -0500

    chore: removed EC2 public IP config rule (put it in rules.tf)

--- modules/security/config_baseline/main.tf
+++ modules/security/config_baseline/main.tf
@@ -80,13 +80,3 @@ resource "aws_config_remediation_configuration" "s3_public_access_block" {
     resource_value = "RESOURCE_ID"
   }
 }
-
-## RULE TO DETECT EC2 INSTANCES WITH PUBLIC IP ADDRESSES
-resource "aws_config_config_rule" "ec2_no_public_ip" {
-  name = "ec2-no-public-ip"
-
-  source {
-    owner             = "AWS"
-    source_identifier = "EC2_INSTANCE_NO_PUBLIC_IP"
-  }
-}
