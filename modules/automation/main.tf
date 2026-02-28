d7d4c8dc903324dfd3f6108d187a61f4bd4eaabb
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-02-28 02:00:38 -0500

    chore: change aws_secretsmanager_secret.threat_intel_api_keys resource to use name_prefix instead of name, so that terraform gives it a truly unique suffix

--- modules/automation/main.tf
+++ modules/automation/main.tf
@@ -277,7 +277,7 @@ resource "aws_lambda_function" "ip_enrichment" {
 
 ### STORE IP ENRICHMENT'S API KEYS IN AWS SECRETS MANAGER
 resource "aws_secretsmanager_secret" "threat_intel_api_keys" {
-  name        = "tf-secure-baseline/threat-intel/api-keys-${var.random_id}"
+  name_prefix        = "tf-secure-baseline/threat-intel/api-keys-"
   description = "API keys for external threat intel providers (AbuseIPDB, VirusTotal, etc.)"
   kms_key_id  = var.lambda_kms_key_arn
 
