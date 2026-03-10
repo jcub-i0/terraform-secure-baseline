07a087e8a3f0c75651af53bb36c8daf73c21d5fd
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-03-09 23:22:09 -0400

    style: terraform fmt

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "main_vpc_cidr" {
 variable "azs" {
   description = "List of Availability Zones for deployment."
   type        = list(string)
-  default     = [
+  default = [
     "us-east-1a",
     "us-east-1b"
   ]
@@ -27,7 +27,7 @@ variable "subnet_cidrs" {
     "compute_private"    = ["10.0.16.0/24", "10.0.17.0/24"]
     "data_private"       = ["10.0.32.0/24", "10.0.33.0/24"]
     "serverless_private" = ["10.0.48.0/24", "10.0.49.0/24"]
-    "firewall_private" = ["10.0.64.0/24", "10.0.65.0/24"]
+    "firewall_private"   = ["10.0.64.0/24", "10.0.65.0/24"]
   }
 }
 
