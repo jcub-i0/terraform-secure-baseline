f9d2064de5a9dc8c749cba3a8e122a605e77813d
jcub <64655784+jcub-i0@users.noreply.github.com>
2026-03-10 21:03:16 -0400

    chore: define firewall_endpoint_ids_by_az output

--- modules/network_firewall/outputs.tf
+++ modules/network_firewall/outputs.tf
@@ -13,3 +13,11 @@ output "firewall_status" {
 output "sync_states" {
   value = aws_networkfirewall_firewall.egress.firewall_status[0].sync_states
 }
+
+output "firewall_endpoint_ids_by_az" {
+  description = "Map of AZ -> Network Firewall endpoint ID"
+  value = {
+    for s in aws_networkfirewall_firewall.egress.firewall_status[0].sync_states :
+    s.availability_zone => s.attachment[0].endpoint_id
+  }
+}
