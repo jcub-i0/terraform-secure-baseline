dfb3dfb6c1645c47118980968b710a96fcb8123e
Jacob Molland <jacobmolland28@gmail.com>
2026-03-12 00:13:10 -0400

    feat: create 'AllowFirewallLogDeliveryWrite' statement to the centralized logs bucket's S3 bucket policy

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -344,6 +344,21 @@ resource "aws_s3_bucket_policy" "centralized_logs" {
             "aws:SourceAccount" = var.account_id
           }
         }
+      },
+      {
+        Sid = "AllowFirewallLogDeliveryWrite"
+        Effect = "Allow"
+        Principal = {
+          Service = "delivery.logs.amazonaws.com"
+        }
+        Action = "s3:PutObject"
+        Resource = "${aws_s3_bucket.centralized_logs.arn}/firewall/flow/*"
+        Condition = {
+          StringEquals = {
+            "aws:SourceAccount" = var.account_id
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
       }
     ]
   })
