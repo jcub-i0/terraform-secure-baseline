d0b359955917bd7f46072074be1ced8b47c35457
Jacob Molland <jacobmolland28@gmail.com>
2026-03-12 00:08:22 -0400

    feat: add 'AllowFirewallLogDeliveryAclCheck' IAM policy to centralized logs S3 bucket policy

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -330,6 +330,20 @@ resource "aws_s3_bucket_policy" "centralized_logs" {
             "s3:x-amz-server-side-encryption" = "aws:kms"
           }
         }
+      },
+      {
+        Sid = "AllowFirewallLogDeliveryAclCheck"
+        Effect = "Allow"
+        Principal = {
+          Service = "delivery.logs.amazonaws.com"
+        }
+        Action = "s3:GetBucketAcl"
+        Resource = aws_s3_bucket.centralized_logs.arn
+        Condition = {
+          StringEquals = {
+            "aws:SourceAccount" = var.account_id
+          }
+        }
       }
     ]
   })
