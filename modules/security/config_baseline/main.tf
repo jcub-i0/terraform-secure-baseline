fb125dacf61d470c51d5cb7375a2f3270df61947
Jacob Molland <jacobmolland28@gmail.com>
2026-03-13 17:25:37 -0400

    refactor: configure configuration recorder to use var.cloud_name

--- modules/security/config_baseline/main.tf
+++ modules/security/config_baseline/main.tf
@@ -4,7 +4,7 @@
 
 ## CONFIGURATION RECORDER
 resource "aws_config_configuration_recorder" "config" {
-  name     = "tf-secure-baseline"
+  name     = var.cloud_name
   role_arn = var.config_role_arn
 
   recording_group {
