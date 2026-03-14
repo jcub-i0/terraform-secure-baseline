ecb3b23088fd69af945b36083f2ae2ad43dba1d1
Jacob Molland <jacobmolland28@gmail.com>
2026-03-13 21:58:30 -0400

    chore: define patch_tag_value variable and add description to 'cloud_name'

--- modules/patch_management/variables.tf
+++ modules/patch_management/variables.tf
@@ -1,3 +1,10 @@
 variable "cloud_name" {
+    description = "Name of the cloud environment"
   type = string
+}
+
+variable "patch_tag_value" {
+  description = "Tag value used to target patchable instances"
+  type = string
+  default = "weekly-linux"
 }
