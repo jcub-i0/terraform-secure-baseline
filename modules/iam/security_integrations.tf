1053108ec6d9bce36f0de8bf9b0a77ca655f347b
Jacob Molland <jacobmolland28@gmail.com>
2026-03-14 00:35:15 -0400

    refactor: add access analyzer and eventbridge iam resources

---
+++ modules/iam/security_integrations.tf
@@ -0,0 +1,44 @@
+#############################################
+# SECURITY SERVICE INTEGRATIONS
+#
+# Roles and policies used by AWS security
+# services to interact with other resources
+# in the environment.
+#############################################
+
+# IAM ACCESS ANALYZER
+resource "aws_accessanalyzer_analyzer" "main" {
+  analyzer_name = "account-access-analyzer"
+  type          = "ACCOUNT"
+
+  tags = {
+    Terraform = "true"
+  }
+}
+
+# EVENTBRIDGE ROLE
+resource "aws_iam_role" "eventbridge_putevents_to_secops" {
+  name = "EventBridgePutEventsToSecopsBus"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "events.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+# ALLOW EVENTBRIDGE TO PUT EVENTS TO SECOPS BUS
+resource "aws_iam_role_policy" "eventbridge_putevents_to_secops" {
+  role = aws_iam_role.eventbridge_putevents_to_secops.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect   = "Allow"
+      Action   = "events:PutEvents"
+      Resource = var.secops_event_bus_arn
+    }]
+  })
+}
