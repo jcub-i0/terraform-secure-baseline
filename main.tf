3c616d8f41d36d123a37bf50c5928397a2eabbb9
Jacob Molland <jacobmolland28@gmail.com>
2026-03-13 23:01:09 -0400

    chore: remove 'patch_tag_key' var from the 'compute' and 'patch_management' modules

--- main.tf
+++ main.tf
@@ -35,7 +35,6 @@ module "compute" {
   interface_endpoints_sg_id      = module.vpc_endpoints.interface_endpoints_sg_id
   data_sg_id                     = module.storage.data_sg_id
   db_port                        = var.db_port
-  patch_tag_key                  = var.patch_tag_key
   patch_tag_value                = var.patch_tag_value
 }
 
@@ -176,6 +175,5 @@ module "patch_management" {
 
   cloud_name                        = var.cloud_name
   patch_maintenance_window_role_arn = module.iam.patch_maintenance_window_role_arn
-  patch_tag_key                     = var.patch_tag_key
   patch_tag_value                   = var.patch_tag_value
 }
