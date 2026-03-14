d2a1bef6a64b58d2045d08d63a7d9c8461256072
Jacob Molland <jacobmolland28@gmail.com>
2026-03-14 00:22:32 -0400

    refactor: cut shared policies from iam/main.tf

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -65,61 +65,6 @@ resource "aws_iam_role_policy" "s3_public_remediation" {
   })
 }
 
-## GENERIC POLICY TO ALLOW READ ACCESS TO CENTRALIZED LOGS S3 BUCKET
-resource "aws_iam_policy" "logs_s3_readonly" {
-  name        = "CentralizedLogsS3ReadOnly"
-  description = "Read-only access to Centralized Logs S3 bucket (no delete, no write)"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      # LIST BUCKET + READ BUCKET METADATA
-      {
-        Sid    = "ListCentralizedLogsBucket"
-        Effect = "Allow"
-        Action = [
-          "s3:ListBucket",
-          "s3:GetBucketLocation"
-        ]
-        Resource = var.centralized_logs_bucket_arn
-      },
-      # READ OBJECTS + VERSIONS
-      {
-        Sid    = "ReadCentralizedLogsObjects"
-        Effect = "Allow"
-        Action = [
-          "s3:GetObject",
-          "s3:GetObjectVersion",
-          "s3:GetObjectTagging",
-          "s3:GetObjectVersionTagging"
-        ]
-        Resource = "${var.centralized_logs_bucket_arn}/*"
-      }
-    ]
-  })
-}
-
-## GENERIC POLICY TO ALLOW DECRYPTION OF OBJECTS ENCRYPTED WITH THE LOGS CMK
-resource "aws_iam_policy" "logs_kms_decrypt" {
-  name        = "LogsKmsDecrypt"
-  description = "Allow decryption of objects encrypted with the Logs CMK"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "AllowDecryptLogsKey"
-        Effect = "Allow"
-        Action = [
-          "kms:Decrypt",
-          "kms:DescribeKey"
-        ]
-        Resource = var.logs_cmk_arn
-      }
-    ]
-  })
-}
-
 ### EC2 ROLLBACK TRIGGER POLICY
 resource "aws_iam_policy" "secops_rollback_trigger" {
   name = "SecOpsRollbackTriggerPolicy"
