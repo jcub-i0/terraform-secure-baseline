3df37d30382ffefc15ac5434480f625442c84f92
Jacob Molland <jacobmolland28@gmail.com>
2026-03-16 18:56:50 -0400

    chore: add ec2_high_critical value to the securityhub_insight_arns map(string) output

--- modules/security_dashboard/outputs.tf
+++ modules/security_dashboard/outputs.tf
@@ -1,10 +1,11 @@
 output "securityhub_insight_arns" {
   description = "ARNs of Security Hub custom insights"
   value = {
-    critical_high   = aws_securityhub_insight.critical_high.arn
+    high_critical   = aws_securityhub_insight.high_critical.arn
     guardduty       = aws_securityhub_insight.guardduty_active.arn
     inspector       = aws_securityhub_insight.inspector_active.arn
     ec2_findings    = aws_securityhub_insight.ec2_findings.arn
+    ec2_high_critical = aws_securityhub_insight.ec2_high_critical.arn
     failed_controls = aws_securityhub_insight.failed_controls.arn
   }
 }
