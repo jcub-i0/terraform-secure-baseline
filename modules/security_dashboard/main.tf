64d764411bd9adc0bd7f0fd2ef7b17ddb6984511
Jacob Molland <jacobmolland28@gmail.com>
2026-03-16 18:10:56 -0400

    style: terraform fmt

--- modules/security_dashboard/main.tf
+++ modules/security_dashboard/main.tf
@@ -29,87 +29,87 @@ resource "aws_securityhub_insight" "critical_high" {
 
 ## GUARDDUTY ACTIVE FINDINGS
 resource "aws_securityhub_insight" "guardduty_active" {
-  name = "Active GuardDuty Findings"
+  name               = "Active GuardDuty Findings"
   group_by_attribute = "SeverityLabel"
 
   filters {
     product_arn {
       comparison = "EQUALS"
-      value = "GuardDuty"
+      value      = "GuardDuty"
     }
 
     workflow_status {
       comparison = "NOT_EQUALS"
-      value = "RESOLVED"
+      value      = "RESOLVED"
     }
 
     record_state {
       comparison = "EQUALS"
-      value = "ACTIVE"
+      value      = "ACTIVE"
     }
   }
 }
 
 ## INSPECTOR ACTIVE FINDINGS
 resource "aws_securityhub_insight" "inspector_active" {
-  name = "Active Inspector Findings"
+  name               = "Active Inspector Findings"
   group_by_attribute = "SeverityLabel"
 
   filters {
     product_name {
       comparison = "EQUALS"
-      value = "INSPECTOR"
+      value      = "INSPECTOR"
     }
 
     workflow_status {
       comparison = "NOT_EQUALS"
-      value = "RESOLVED"
+      value      = "RESOLVED"
     }
 
     record_state {
       comparison = "EQUALS"
-      value = "ACTIVE"
+      value      = "ACTIVE"
     }
   }
 }
 
 ## EC2 FINDINGS
 resource "aws_securityhub_insight" "ec2_findings" {
-  name = "EC2 Findings"
+  name               = "EC2 Findings"
   group_by_attribute = "ResourceId"
 
   filters {
     product_arn {
       comparison = "EQUALS"
-      value = "AwsEc2Instance"
+      value      = "AwsEc2Instance"
     }
 
     workflow_status {
       comparison = "NOT_EQUALS"
-      value = "RESOLVED"
+      value      = "RESOLVED"
     }
 
     record_state {
       comparison = "EQUALS"
-      value = "ACTIVE"
+      value      = "ACTIVE"
     }
   }
 }
 
 ## FAILED CONTROLS
 resource "aws_securityhub_insight" "failed_controls" {
-  name = "Failed Controls by Standard"
+  name               = "Failed Controls by Standard"
   group_by_attribute = "ComplianceSecurityControlId"
 
   filters {
     compliance_status {
       comparison = "EQUALS"
-      value = "FAILED"
+      value      = "FAILED"
     }
 
     record_state {
       comparison = "EQUALS"
-      value = "ACTIVE"
+      value      = "ACTIVE"
     }
   }
 }
