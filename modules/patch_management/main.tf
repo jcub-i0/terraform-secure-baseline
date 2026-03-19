fb39335fecacabd889bfe5ee198f72d0580e9715
Jacob Molland <jacobmolland28@gmail.com>
2026-03-18 22:25:57 -0400

    chore: remove locals block and reference var.name_prefix in the patch_management module

--- modules/patch_management/main.tf
+++ modules/patch_management/main.tf
@@ -1,10 +1,6 @@
-locals {
-  name_prefix = "${var.cloud_name}-${var.environment}"
-}
-
 # MAINTENANCE WINDOW
 resource "aws_ssm_maintenance_window" "patching" {
-  name                       = "${local.name_prefix}--weekly-patching"
+  name                       = "${var.name_prefix}--weekly-patching"
   description                = "Weekly patch window for Ubuntu EC2 instances"
   schedule                   = var.patch_schedule
   schedule_timezone          = var.schedule_timezone
@@ -14,7 +10,7 @@ resource "aws_ssm_maintenance_window" "patching" {
   enabled                    = var.patching_enabled
 
   tags = {
-    Name        = "${local.name_prefix}-Weekly-Patching"
+    Name        = "${var.name_prefix}-Weekly-Patching"
     Environment = var.environment
     Terraform   = "true"
   }
