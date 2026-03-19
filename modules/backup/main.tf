60c4223292930888612585fd0854e4aa5968fd04
Jacob Molland <jacobmolland28@gmail.com>
2026-03-18 23:46:08 -0400

    feat: define aws_backup_vault

--- modules/backup/main.tf
+++ modules/backup/main.tf
@@ -1,2 +1,12 @@
 # BACKUP / DISASTER RECOVERY RESOURCES
 
+resource "aws_backup_vault" "main" {
+  name = "${var.name_prefix}-backup-vault"
+  kms_key_arn = var.backup_vault_cmk_arn
+
+  tags = {
+    Name = "${var.name_prefix}-daily-backup"
+    Environment = var.environment
+    Terraform = "true"
+  }
+}
