e0065876c48742a0a6eeda0e0e0478c2bac1fed7
Jacob Molland <jacobmolland28@gmail.com>
2026-03-20 18:22:57 -0400

    feat: create cloudwatch log group for rds upgrade logs

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -70,7 +70,7 @@ resource "aws_db_instance" "main" {
   }
 }
 
-# CLOUDWATCH LOG GROUP FOR RDS
+# CLOUDWATCH LOG GROUP FOR RDS POSTGRESQL LOGS
 resource "aws_cloudwatch_log_group" "rds_postgresql" {
   name = "/aws/rds/instance/${aws_db_instance.main.identifier}/postgresql"
   retention_in_days = 30
@@ -78,6 +78,19 @@ resource "aws_cloudwatch_log_group" "rds_postgresql" {
 
   tags = {
     Name = "${var.name_prefix}-rds-postgresql-logs"
+    Environment = var.environment
+    Terraform = "true"
+  }
+}
+
+# CLOUDWATCH LOG GROUP FOR RDS POSTGRESQL UDGRADE LOGS
+resource "aws_cloudwatch_log_group" "rds_upgrade" {
+  name = "/aws/rds/instance/${aws_db_instance.main.identifier}/upgrade"
+  retention_in_days = 30
+  kms_key_id = var.logs_cmk_arn
+
+  tags = {
+    Name = "${var.name_prefix}-rds-upgrade-logs"
     Environment = var.environment
     Terraform = "true"
   }
