a55521013d2b84fa495f7d91dddcade830452e41
Jacob Molland <jacobmolland28@gmail.com>
2026-03-20 19:57:30 -0400

    chore: define secops analyst, engineer, and operator group name variables

---
+++ modules/identity_center/variables.tf
@@ -0,0 +1,14 @@
+variable "secops_analyst_group_name" {
+  description = "Name of the SecOps-Analyst IAM group"
+  type = string
+}
+
+variable "secops_engineer_group_name" {
+  description = "Name of the SecOps-Engineer IAM group"
+  type = string
+}
+
+variable "secops_operator_group_name" {
+  description = "Name of the SecOps-Operator IAM group"
+  type = string
+}
