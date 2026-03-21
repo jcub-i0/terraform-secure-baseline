f15d108134d5a0117121523794235d72e5049952
Jacob Molland <jacobmolland28@gmail.com>
2026-03-20 21:15:34 -0400

    chore: attach readonly aws-managed role policy to secops-engineer group

--- modules/identity_center/main.tf
+++ modules/identity_center/main.tf
@@ -95,6 +95,12 @@ resource "aws_ssoadmin_managed_policy_attachment" "secops_engineer_security_audi
   managed_policy_arn = "arn:aws:iam::aws:policy/SecurityAudit"
 }
 
+resource "aws_ssoadmin_managed_policy_attachment" "secops_engineer_readonly" {
+  instance_arn = local.instance_arn
+  permission_set_arn = aws_ssoadmin_permission_set.secops_engineer.arn
+  managed_policy_arn = "arn:aws:iam::aws:policy/SecurityAudit"
+}
+
 ##########################################
 # INLINE POLICY FOR SECOPS-ENGINEER
 ##########################################
