342ed88feb079e5f820c7c6b119c1089f1e344b8
Jacob Molland <jacobmolland28@gmail.com>
2026-03-20 22:35:37 -0400

    chore: attach customer-managed policies to secops_operator group

--- modules/identity_center/main.tf
+++ modules/identity_center/main.tf
@@ -145,7 +145,15 @@ resource "aws_ssoadmin_customer_managed_policy_attachment" "secops_engineer_logs
   }
 }
 
+resource "aws_ssoadmin_customer_managed_policy_attachment" "secops_operator_rollback_trigger" {
+  instance_arn       = local.instance_arn
+  permission_set_arn = aws_ssoadmin_permission_set.secops_operator.arn
 
+  customer_managed_policy_reference {
+    name = var.secops_rollback_trigger_policy_name
+    path = var.customer_managed_policy_path
+  }
+}
 
 ##########################################
 # INLINE POLICY FOR SECOPS-ENGINEER
