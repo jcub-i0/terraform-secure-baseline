e84018b951c4486ebadfc9674b6df68ed65b5e54
Jacob Molland <jacobmolland28@gmail.com>
2026-03-20 22:25:18 -0400

    chore: define logs s3 readonly, logs cmk decrypt, and secops rollback trigger policy variables in the iam module

--- modules/identity_center/variables.tf
+++ modules/identity_center/variables.tf
@@ -17,3 +17,18 @@ variable "account_id" {
   description = ""
   type        = string
 }
+
+variable "logs_s3_readonly_policy_name" {
+    description = "'Name' attribute of the logs_s3_readonly policy"
+  type = string
+}
+
+variable "logs_cmk_decrypt_policy_name" {
+  description = "'Name' attribute of the logs_cmk_decrypt policy"
+  type = string
+}
+
+variable "secops_rollback_trigger_policy_name" {
+  description = "'Name' attribute of the secops_rollback_trigger policy"
+  type = string
+}
