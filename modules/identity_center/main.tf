df6826835aab3364eea6d87fccaa2263a66f3daf
Jacob Molland <jacobmolland28@gmail.com>
2026-03-20 20:02:20 -0400

    feat: create secops-analysts group

--- modules/identity_center/main.tf
+++ modules/identity_center/main.tf
@@ -1,1 +1,27 @@
 # IAM IDENTITY CENTER (SSO) RESOURCES
+
+##########################################
+# IAM IDENTITY CENTER - DISCOVER INSTANCE
+##########################################
+
+data "aws_ssoadmin_instances" "this" {}
+
+locals {
+  instance_arn = tolist(data.aws_ssoadmin_instances.this.arns)[0]
+  identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
+}
+
+##########################################
+# GROUP LOOKUPS
+##########################################
+
+data "aws_identitystore_group" "analysts" {
+  identity_store_id = local.identity_store_id
+
+  alternate_identifier {
+    unique_attribute {
+      attribute_path = "Displayname"
+      attribute_value = var.secops_analyst_group_name
+    }
+  }
+}
