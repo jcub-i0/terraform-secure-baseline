ed1dcd06a0f0db5e04e0a14a4a253dc455eea441
Jacob Molland <jacobmolland28@gmail.com>
2026-03-23 18:00:54 -0400

    chore: left some comments for clarity/organization

--- modules/identity_center/main.tf
+++ modules/identity_center/main.tf
@@ -77,6 +77,8 @@ resource "aws_ssoadmin_permission_set" "secops_operator" {
 # AWS-MANAGED POLICY ATTACHMENTS
 ##########################################
 
+# SECOPS-ANALYST POLICY ATTACHMENTS
+
 resource "aws_ssoadmin_managed_policy_attachment" "secops_analyst_security_audit" {
   instance_arn       = local.instance_arn
   permission_set_arn = aws_ssoadmin_permission_set.secops_analyst.arn
@@ -88,6 +90,8 @@ resource "aws_ssoadmin_managed_policy_attachment" "secops_analyst_readonly" {
   permission_set_arn = aws_ssoadmin_permission_set.secops_analyst.arn
   managed_policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
 }
+
+# SECOPS-ENGINEER POLICY ATTACHMENTS
 
 resource "aws_ssoadmin_managed_policy_attachment" "secops_engineer_security_audit" {
   instance_arn       = local.instance_arn
@@ -104,6 +108,8 @@ resource "aws_ssoadmin_managed_policy_attachment" "secops_engineer_readonly" {
 ##########################################
 # CUSTOMER-MANAGED POLICY ATTACHMENTS
 ##########################################
+
+# SECOPS-OPERATOR POLICY ATTACHMENTS
 
 resource "aws_ssoadmin_customer_managed_policy_attachment" "secops_analyst_logs_s3" {
   instance_arn       = local.instance_arn
@@ -125,6 +131,8 @@ resource "aws_ssoadmin_customer_managed_policy_attachment" "secops_analyst_logs_
   }
 }
 
+# SECOPS-ENGINEER POLICY ATTACHMENTS
+
 resource "aws_ssoadmin_customer_managed_policy_attachment" "secops_engineer_logs_s3" {
   instance_arn       = local.instance_arn
   permission_set_arn = aws_ssoadmin_permission_set.secops_engineer.arn
@@ -144,6 +152,8 @@ resource "aws_ssoadmin_customer_managed_policy_attachment" "secops_engineer_logs
     path = var.customer_managed_policy_path
   }
 }
+
+# SECOPS-OPERATOR POLICY ATTACHMENTS
 
 resource "aws_ssoadmin_customer_managed_policy_attachment" "secops_operator_rollback_trigger" {
   instance_arn       = local.instance_arn
