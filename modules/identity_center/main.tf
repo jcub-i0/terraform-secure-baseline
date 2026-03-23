4c349e1c94da58dd254b53d927f935430d354aec
Jacob Molland <jacobmolland28@gmail.com>
2026-03-23 17:57:20 -0400

    fix: update secops_analysts identitystore_group resource's 'attribute_path' to reference 'DisplayName' vs 'Displayname'

--- modules/identity_center/main.tf
+++ modules/identity_center/main.tf
@@ -20,7 +20,7 @@ data "aws_identitystore_group" "secops_analysts" {
 
   alternate_identifier {
     unique_attribute {
-      attribute_path  = "Displayname"
+      attribute_path  = "DisplayName"
       attribute_value = var.secops_analyst_group_name
     }
   }
