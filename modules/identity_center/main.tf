4f0d4e31f0c0068a383fe90fc1bac5dcbfef4a52
Jacob Molland <jacobmolland28@gmail.com>
2026-03-23 18:02:44 -0400

    fix: modify 'secops_engineer_readonly' policy attachment to reference the 'ReadOnlyAccess' policy's ARN vs the 'SecurityAdutit' ARN

--- modules/identity_center/main.tf
+++ modules/identity_center/main.tf
@@ -102,7 +102,7 @@ resource "aws_ssoadmin_managed_policy_attachment" "secops_engineer_security_audi
 resource "aws_ssoadmin_managed_policy_attachment" "secops_engineer_readonly" {
   instance_arn       = local.instance_arn
   permission_set_arn = aws_ssoadmin_permission_set.secops_engineer.arn
-  managed_policy_arn = "arn:aws:iam::aws:policy/SecurityAudit"
+  managed_policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
 }
 
 ##########################################
