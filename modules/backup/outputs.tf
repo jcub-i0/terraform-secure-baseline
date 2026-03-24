59e3da512685e283816db5cd689541f3d126a7b3
Jacob Molland <jacobmolland28@gmail.com>
2026-03-23 23:38:50 -0400

    style: terraform fmt

--- modules/backup/outputs.tf
+++ modules/backup/outputs.tf
@@ -1,7 +1,7 @@
 output "permission_set_arns" {
   description = "Permission set ARNs"
   value = {
-    analyst = aws_ssoadmin_permission_set.analyst.arn
+    analyst  = aws_ssoadmin_permission_set.analyst.arn
     engineer = aws_ssoadmin_permission_set.engineer.arn
   }
 }
