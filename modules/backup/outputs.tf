7e65c59945c53d635a0eda1f72a780b3b0e8226e
Jacob Molland <jacobmolland28@gmail.com>
2026-03-23 23:38:29 -0400

    chore: define 'permission_set_arns' output in the 'backup' module

---
+++ modules/backup/outputs.tf
@@ -0,0 +1,7 @@
+output "permission_set_arns" {
+  description = "Permission set ARNs"
+  value = {
+    analyst = aws_ssoadmin_permission_set.analyst.arn
+    engineer = aws_ssoadmin_permission_set.engineer.arn
+  }
+}
