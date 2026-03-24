d54a8ae721e2d4eb676bb6c14f97408d0f22e288
Jacob Molland <jacobmolland28@gmail.com>
2026-03-24 00:07:53 -0400

    fix: remove incorrect output defined in 'backup' module

--- modules/backup/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "permission_set_arns" {
-  description = "Permission set ARNs"
-  value = {
-    analyst  = aws_ssoadmin_permission_set.analyst.arn
-    engineer = aws_ssoadmin_permission_set.engineer.arn
-  }
-}
