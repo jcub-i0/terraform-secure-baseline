38419b1fa96fbe9703ce223f5571dd4ee52832be
Jacob Molland <jacobmolland28@gmail.com>
2026-03-24 00:16:45 -0400

    chore: define 'backup_plan_id' output in the 'backup' module

--- modules/backup/outputs.tf
+++ modules/backup/outputs.tf
@@ -1,4 +1,9 @@
 output "backup_vault_name" {
-    description = "The 'Name' attribute of the Main AWS Backup Vault"
+    description = "The 'name' attribute of the Main AWS Backup Vault"
     value = aws_backup_vault.main.name
+}
+
+output "backup_plan_id" {
+  description = "The 'id' attribute of the Main Backup Plan"
+  value = aws_backup_plan.main.id
 }
