b192fc95c33ef077f68622b2dc613bca7baab2a4
Jacob Molland <jacobmolland28@gmail.com>
2026-03-25 22:57:35 -0400

    style: terraform fmt

--- main.tf
+++ main.tf
@@ -237,14 +237,14 @@ module "backup" {
 module "identity_center" {
   source = "./modules/identity_center"
 
-  account_id                          = data.aws_caller_identity.current.account_id
-  secops_analyst_group_name           = "SecOps-Analysts"
-  secops_engineer_group_name          = "SecOps-Engineers"
-  secops_operator_group_name          = "SecOps-Operators"
-  logs_cmk_decrypt_policy_name        = module.iam.logs_cmk_decrypt_policy_name
-  logs_s3_readonly_policy_name        = module.iam.logs_s3_readonly_policy_name
-  secops_event_bus_arn                = module.automation.secops_event_bus_arn
-  customer_managed_policy_path        = "/"
+  account_id                   = data.aws_caller_identity.current.account_id
+  secops_analyst_group_name    = "SecOps-Analysts"
+  secops_engineer_group_name   = "SecOps-Engineers"
+  secops_operator_group_name   = "SecOps-Operators"
+  logs_cmk_decrypt_policy_name = module.iam.logs_cmk_decrypt_policy_name
+  logs_s3_readonly_policy_name = module.iam.logs_s3_readonly_policy_name
+  secops_event_bus_arn         = module.automation.secops_event_bus_arn
+  customer_managed_policy_path = "/"
 
   depends_on = [
     module.iam
