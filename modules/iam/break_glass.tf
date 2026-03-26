f5fc3f9c79b1f460dda5568a0bd9881f31f9bc6c
Jacob Molland <jacobmolland28@gmail.com>
2026-03-25 23:38:00 -0400

    feat: create 'break_glass.tf' file in iam

---
+++ modules/iam/break_glass.tf
@@ -0,0 +1,6 @@
+##########################################
+# BREAK-GLASS ACCESS
+##########################################
+# This role is intended for emergency use only.
+# It should only be used if IAM Identity Center (SSO) is unavailable.
+# All usage should be monitored and audited.
