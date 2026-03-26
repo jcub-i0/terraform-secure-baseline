2b8c2bc34345991f13e56d06a9b438d592e8ea19
Jacob Molland <jacobmolland28@gmail.com>
2026-03-25 23:47:05 -0400

    chore: pass the 'environment' variable into the 'iam' module

--- main.tf
+++ main.tf
@@ -81,6 +81,7 @@ module "iam" {
   source = "./modules/iam"
 
   cloud_name                            = var.cloud_name
+  environment = var.environment
   cloudtrail_log_group_arn              = module.logging.cloudtrail_log_group_arn
   secops_topic_arn                      = module.monitoring.secops_topic_arn
   logs_cmk_arn                          = module.security.logs_cmk_arn
