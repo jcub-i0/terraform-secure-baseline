f990f46e66adefb6e5e50383677b12f971770a6b
Jacob Molland <jacobmolland28@gmail.com>
2026-03-27 17:12:05 -0400

    style: terraform fmt

--- modules/security/outputs.tf
+++ modules/security/outputs.tf
@@ -40,7 +40,7 @@ output "backup_vault_cmk_alias_arn" {
 
 output "securityhub_inspector_high_critical_rule_arn" {
   description = "ARN of the SecurityHub/Inspector High/Critical eventbridge rule target"
-  value = aws_cloudwatch_event_rule.securityhub_inspector_high_critical.arn
+  value       = aws_cloudwatch_event_rule.securityhub_inspector_high_critical.arn
 }
 
 ##################################
