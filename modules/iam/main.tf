57ca47f3cf5d4a3a0d09be568025a511fe80289e
Jacob Molland <jacobmolland28@gmail.com>
2026-03-30 23:53:56 -0400

    feat: define 'github_oidc' child module in the 'iam' module

---
+++ modules/iam/main.tf
@@ -0,0 +1,12 @@
+module "github_oidc" {
+  source = "./github_oidc"
+  count = var.enable_github_oidc ? 1 : 0
+
+  github_owner = var.github_owner
+  github_repo = var.github_repo
+  github_branches = var.github_branches
+  github_allow_pull_requests = var.github_allow_pull_requests
+  github_oidc_provider_arn = var.github_oidc_provider_arn
+  name_prefix = var.name_prefix
+  tf_state_bucket_arn = var.tf_state_bucket_arn
+}
