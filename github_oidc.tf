70cfe53286948e78888f34ea4c709008a036a90c
Jacob Molland <jacobmolland28@gmail.com>
2026-03-31 00:51:47 -0400

    chore: move aws_iam_openid_connect_provider.github resource into /github_oidc.tf

---
+++ github_oidc.tf
@@ -0,0 +1,13 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  count = var.enable_github_oidc ? 1 : 0
+
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+}
