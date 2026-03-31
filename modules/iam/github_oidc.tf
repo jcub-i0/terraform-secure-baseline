4471582d12588d58954a3e71b794ebedf3e3746e
Jacob Molland <jacobmolland28@gmail.com>
2026-03-30 23:08:06 -0400

    chore: cut the github_oidc_provider resource from iam/github_oidc.tf

--- modules/iam/github_oidc.tf
+++ modules/iam/github_oidc.tf
@@ -10,18 +10,6 @@ locals {
   github_pr_subject = "repo:${var.github_owner}/${var.github_repo}:pull_request"
 }
 
-resource "aws_iam_openid_connect_provider" "github" {
-  url = "https://token.actions.githubusercontent.com"
-
-  client_id_list = [
-    "sts.amazonaws.com"
-  ]
-
-  thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1"
-  ]
-}
-
 data "aws_iam_policy_document" "github_oidc_assume_role" {
   statement {
     effect  = "Allow"
