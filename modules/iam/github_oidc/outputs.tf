d76533004ad95a49e9d596f0fcf737d56e39e27e
Jacob Molland <jacobmolland28@gmail.com>
2026-03-30 23:59:30 -0400

    style: terraform fmt

--- modules/iam/github_oidc/outputs.tf
+++ modules/iam/github_oidc/outputs.tf
@@ -1,4 +1,4 @@
 output "github_plan_role_arn" {
   description = "ARN of the GitHub OIDC Terraform plan role"
-  value = var.enable_github_oidc ? aws_iam_role.github_plan.arn : null
+  value       = var.enable_github_oidc ? aws_iam_role.github_plan.arn : null
 }
