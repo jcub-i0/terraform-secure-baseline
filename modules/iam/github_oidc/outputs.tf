9ee0635f746e3187cbc99bc1aca2add595f86c13
Jacob Molland <jacobmolland28@gmail.com>
2026-04-03 19:56:00 -0400

    fix: add index to 'github_apply_role_arn' output in the 'github_oidc' module

--- modules/iam/github_oidc/outputs.tf
+++ modules/iam/github_oidc/outputs.tf
@@ -5,5 +5,5 @@ output "github_plan_role_arn" {
 
 output "github_apply_role_arn" {
   description = "ARN of the GitHub OIDC Terraform apply role"
-  value       = aws_iam_role.github_apply.arn
+  value       = aws_iam_role.github_apply[0].arn
 }
