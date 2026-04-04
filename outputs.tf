54442f2cc6c88828678eff3ce88de736cead44c2
Jacob Molland <jacobmolland28@gmail.com>
2026-04-04 12:48:49 -0400

    chore: update outputs to reflect the new 'github_oidc' module

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "github_plan_role_arn" {
   description = "ARN of the GitHub OIDC Terraform plan role"
-  value       = module.iam.github_plan_role_arn
+  value       = module.github_oidc.github_plan_role_arn
 }
 
 output "github_apply_role_arn" {
   description = "ARN of the GitHub OIDC Terraform apply role"
-  value       = module.iam.github_apply_role_arn
+  value       = module.github_oidc.github_apply_role_arn
 }
