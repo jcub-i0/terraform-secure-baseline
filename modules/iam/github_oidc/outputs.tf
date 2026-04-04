1f2aa6ac546017587e473e7492dbf328066cb96d
Jacob Molland <jacobmolland28@gmail.com>
2026-04-03 20:02:43 -0400

    fix: correct value for 'github_apply_role_arn' output in 'github_oidc' child module

--- modules/iam/github_oidc/outputs.tf
+++ modules/iam/github_oidc/outputs.tf
@@ -5,5 +5,5 @@ output "github_plan_role_arn" {
 
 output "github_apply_role_arn" {
   description = "ARN of the GitHub OIDC Terraform plan role"
-  value       = var.enable_apply_role_github ? module.github_oidc[0].github_apply_role_arn : null
+  value       = var.enable_apply_role_github ? aws_iam_role.github_apply[0].arn : null
 }
