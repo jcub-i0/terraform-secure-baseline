e2d9894989b8549444f8158c1042f6649b18f385
Jacob Molland <jacobmolland28@gmail.com>
2026-04-06 19:37:10 -0400

    chore: add policies allowing the github_apply and github_plan roles to use the lambda and secrets manager CMKs

--- modules/github_oidc/main.tf
+++ modules/github_oidc/main.tf
@@ -113,6 +113,28 @@ resource "aws_iam_policy" "github_plan" {
           ]
           Resource = "*"
         },
+        {
+          Sid    = "SecretsManagerKmsDecrypt"
+          Effect = "Allow"
+          Action = [
+            "kms:Decrypt",
+            "kms:DescribeKey"
+          ]
+          Resource = [
+            var.secrets_manager_cmk_arn
+          ]
+        },
+        {
+          Sid    = "LambdaKmsDecrypt"
+          Effect = "Allow"
+          Action = [
+            "kms:Decrypt",
+            "kms:DescribeKey"
+          ]
+          Resource = [
+            var.lambda_cmk_arn
+          ]
+        }
       ],
       var.tf_state_lock_table_arn != null ? [
         {
@@ -242,6 +264,28 @@ resource "aws_iam_policy" "github_apply" {
           ]
           Resource = "*"
         },
+        {
+          Sid    = "SecretsManagerKmsDecrypt"
+          Effect = "Allow"
+          Action = [
+            "kms:Decrypt",
+            "kms:DescribeKey"
+          ]
+          Resource = [
+            var.secrets_manager_cmk_arn
+          ]
+        },
+        {
+          Sid    = "LambdaKmsDecrypt"
+          Effect = "Allow"
+          Action = [
+            "kms:Decrypt",
+            "kms:DescribeKey"
+          ]
+          Resource = [
+            var.lambda_cmk_arn
+          ]
+        }
       ],
       var.tf_state_lock_table_arn != null ? [
         {
