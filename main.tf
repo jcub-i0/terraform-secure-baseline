c3f3eb8c3ca0487d8b2b36f63a65202a179d1851
Jacob Molland <jacobmolland28@gmail.com>
2026-04-06 20:33:01 -0400

    style: terraform fmt

--- main.tf
+++ main.tf
@@ -265,19 +265,19 @@ module "github_oidc" {
   count  = var.enable_github_oidc ? 1 : 0
 
   prevent_destroy_github_oidc_resources = var.prevent_destroy_github_oidc_resources
-  owner_github                    = var.owner_github
-  repo_github                     = var.repo_github
-  branches_plan_github            = var.branches_plan_github
-  allow_pull_requests_plan_github = var.allow_pull_requests_plan_github
-  name_prefix                     = local.name_prefix
-  tf_state_bucket_arn             = var.tf_state_bucket_arn
-  tf_state_bucket_cmk_arn         = var.tf_state_bucket_cmk_arn
-  tf_state_lock_table_arn         = var.tf_state_lock_table_arn
-  primary_region                  = var.primary_region
-  account_id                      = data.aws_caller_identity.current.account_id
-  enable_apply_role_github        = var.enable_apply_role_github
-  branches_apply_github           = var.branches_apply_github
-  environment_apply_github        = var.environment_apply_github
-  lambda_cmk_arn                  = module.security.lambda_cmk_arn
-  secrets_manager_cmk_arn         = module.security.secrets_manager_cmk_arn
+  owner_github                          = var.owner_github
+  repo_github                           = var.repo_github
+  branches_plan_github                  = var.branches_plan_github
+  allow_pull_requests_plan_github       = var.allow_pull_requests_plan_github
+  name_prefix                           = local.name_prefix
+  tf_state_bucket_arn                   = var.tf_state_bucket_arn
+  tf_state_bucket_cmk_arn               = var.tf_state_bucket_cmk_arn
+  tf_state_lock_table_arn               = var.tf_state_lock_table_arn
+  primary_region                        = var.primary_region
+  account_id                            = data.aws_caller_identity.current.account_id
+  enable_apply_role_github              = var.enable_apply_role_github
+  branches_apply_github                 = var.branches_apply_github
+  environment_apply_github              = var.environment_apply_github
+  lambda_cmk_arn                        = module.security.lambda_cmk_arn
+  secrets_manager_cmk_arn               = module.security.secrets_manager_cmk_arn
 }
