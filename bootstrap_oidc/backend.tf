82021f6b5108871e591750c00be4bce573f4c309
Jacob Molland <jacobmolland28@gmail.com>
2026-04-08 17:24:16 -0400

    chore: change 'key' of remote s3 backend for the bootstrap stack to use 'tf-state-bootstrap' instead of 'baseline-tf-state/tf-state-bootstrap'

--- bootstrap_oidc/backend.tf
+++ bootstrap_oidc/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "baseline-tf-state"
-    key     = "baseline-tf-state/tf-state-bootstrap"
+    key     = "tf-state-bootstrap"
     region  = "us-east-1"
     encrypt = true
   }
