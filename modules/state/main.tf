9c622ab0ddea68598eb101d4e1ef5e5791183d51
Jacob Molland <jacobmolland28@gmail.com>
2026-04-10 15:13:38 -0400

    feat: create state S3 bucket

--- modules/state/main.tf
+++ modules/state/main.tf
@@ -1,3 +1,22 @@
 ###############
 # STATE MODULE
 ###############
+
+locals {
+  name_prefix = "${var.cloud_name}-${var.environment}"
+}
+
+resource "aws_s3_bucket" "state_bucket" {
+  bucket = "${var.cloud_name}-state"
+  object_lock_enabled = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    Name = "${var.cloud_name}-State"
+    Environment = var.environment
+    Terraform = "true"
+  }
+}
