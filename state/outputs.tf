d459bade14a182f1ee96f150b8d63003dfc625f5
Jacob Molland <jacobmolland28@gmail.com>
2026-04-10 17:49:17 -0400

    chore: define the 'tf_state_bucket_arn' output from the 'state' stack

--- state/outputs.tf
+++ state/outputs.tf
@@ -1,3 +1,8 @@
+output "tf_state_bucket_arn" {
+  description = "ARN of the S3 bucket where the Terraform state is stored"
+  value = module.state.tf_state_bucket_arn
+}
+
 output "tf_state_lock_table_arn" {
   description = "ARN of the State Lock DynamoDB table used for Terraform state locking"
   value       = module.state.tf_state_lock_table_arn
