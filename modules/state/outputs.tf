0838bdedeb73aac0bc99cfa5dd5db30a5068d6a1
Jacob Molland <jacobmolland28@gmail.com>
2026-04-10 18:37:19 -0400

    chore: define 'tf_state_bucket_name' output in the 'state' module

--- modules/state/outputs.tf
+++ modules/state/outputs.tf
@@ -3,6 +3,11 @@ output "tf_state_bucket_arn" {
   value       = aws_s3_bucket.state.arn
 }
 
+output "tf_state_bucket_name" {
+  description = "Name of the S3 bucket where the Terraform state is stored"
+  value = aws_s3_bucket.state.bucket
+}
+
 output "tf_state_bucket_cmk_arn" {
   description = "ARN of the KMS CMK used to encrypt the Terraform State bucket"
   value       = aws_kms_key.state.arn
