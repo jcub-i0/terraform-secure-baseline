a05ad0580a7a4ccb22c0eb630c000f12b2d47737
Jacob Molland <jacobmolland28@gmail.com>
2026-04-10 17:49:34 -0400

    style: terraform fmt

--- state/outputs.tf
+++ state/outputs.tf
@@ -1,6 +1,6 @@
 output "tf_state_bucket_arn" {
   description = "ARN of the S3 bucket where the Terraform state is stored"
-  value = module.state.tf_state_bucket_arn
+  value       = module.state.tf_state_bucket_arn
 }
 
 output "tf_state_lock_table_arn" {
