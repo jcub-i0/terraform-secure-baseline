a0c55e2ac2310d916e991631613acbb2fce67c55
Jacob Molland <jacobmolland28@gmail.com>
2026-04-10 18:44:02 -0400

    chore: add 'tf_state_lock_table_arn' output from the 'state' stack

--- state/outputs.tf
+++ state/outputs.tf
@@ -3,12 +3,17 @@ output "tf_state_bucket_arn" {
   value       = module.state.tf_state_bucket_arn
 }
 
-output "tf_state_lock_table_arn" {
-  description = "ARN of the State Lock DynamoDB table used for Terraform state locking"
-  value       = module.state.tf_state_lock_table_arn
+output "tf_state_bucket_name" {
+  description = "Name of the S3 bucket where the Terraform state is stored"
+  value       = module.state.tf_state_bucket_name
 }
 
 output "tf_state_bucket_cmk_arn" {
   description = "ARN of the KMS CMK used to encrypt the Terraform State bucket"
   value       = module.state.tf_state_bucket_cmk_arn
+}
+
+output "tf_state_lock_table_arn" {
+  description = "ARN of the State Lock DynamoDB table used for Terraform state locking"
+  value       = module.state.tf_state_lock_table_arn
 }
