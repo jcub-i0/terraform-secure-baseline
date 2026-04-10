675690d5b4f48e37b7f05c4dc6f21f5f5b208b12
Jacob Molland <jacobmolland28@gmail.com>
2026-04-10 17:23:26 -0400

    chore: define 'tf_state_lock_table_arn' output from the 'state' module

--- state/outputs.tf
+++ state/outputs.tf
@@ -1,4 +1,4 @@
 output "tf_state_lock_table_arn" {
   description = "ARN of the State Lock DynamoDB table used for Terraform state locking"
-  value       = module.state.state_lock_dynamodb_table_arn
+  value       = module.state.tf_state_lock_table_arn
 }
