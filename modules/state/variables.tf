d14605d0c57ce3351648116ee9b73192823c2f66
Jacob Molland <jacobmolland28@gmail.com>
2026-04-10 15:08:33 -0400

    chore: define 'cloud_name', 'environment', and 'primary_region' variables in 'state' module

---
+++ modules/state/variables.tf
@@ -0,0 +1,16 @@
+variable "cloud_name" {
+  description = "The name of this cloud environment"
+  type        = string
+  default     = "tf-secure-baseline"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "primary_region" {
+  type    = string
+  default = "us-east-1"
+}
