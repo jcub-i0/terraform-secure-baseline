ec666820d8a4ebd4e9d3e2850b73f9b8f1b3bab5
Jacob Molland <jacobmolland28@gmail.com>
2026-04-10 18:34:38 -0400

    refactor: change order of outputs in 'state' module

--- modules/state/outputs.tf
+++ modules/state/outputs.tf
@@ -3,12 +3,12 @@ output "tf_state_bucket_arn" {
   value       = aws_s3_bucket.state.arn
 }
 
-output "tf_state_lock_table_arn" {
-  description = "ARN of the State Lock DynamoDB table"
-  value       = aws_dynamodb_table.state_lock.arn
-}
-
 output "tf_state_bucket_cmk_arn" {
   description = "ARN of the KMS CMK used to encrypt the Terraform State bucket"
   value       = aws_kms_key.state.arn
+}
+
+output "tf_state_lock_table_arn" {
+  description = "ARN of the State Lock DynamoDB table"
+  value       = aws_dynamodb_table.state_lock.arn
 }
