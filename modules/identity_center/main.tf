7e8cc839a2b3946a8f90116ad0afa084896f5d19
Jacob Molland <jacobmolland28@gmail.com>
2026-04-13 18:24:26 -0400

    chore: add 'depends_on' clause to the 'engineers' account assignment resource so that Terraform knowsn the assignment is the last thing created for that permission set and therefore the first thing destroyed

--- modules/identity_center/main.tf
+++ modules/identity_center/main.tf
@@ -229,6 +229,14 @@ resource "aws_ssoadmin_account_assignment" "engineers" {
 
   target_id   = var.account_id
   target_type = "AWS_ACCOUNT"
+
+  depends_on = [
+    aws_ssoadmin_managed_policy_attachment.secops_engineer_readonly,
+    aws_ssoadmin_managed_policy_attachment.secops_engineer_security_audit,
+    aws_ssoadmin_customer_managed_policy_attachment.secops_engineer_logs_s3,
+    aws_ssoadmin_customer_managed_policy_attachment.secops_engineer_logs_cmk,
+    aws_ssoadmin_permission_set_inline_policy.secops_engineer_inline
+  ]
 }
 
 resource "aws_ssoadmin_account_assignment" "operators" {
