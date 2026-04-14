7aaa64492ac392ba75fe329d436fa6105a34feb9
Jacob Molland <jacobmolland28@gmail.com>
2026-04-14 01:06:47 -0400

    fix: update key of prod's backend

--- baseline/envs/prod/backend.tf
+++ baseline/envs/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tf-secure-baseline-state"
-    key            = "tf-state-baseline-dev"
+    key            = "tf-state-baseline-prod"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "tf-secure-baseline-lock"
