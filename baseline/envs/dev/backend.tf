b6c40a3c07e74c608d2b3d2ea77c7d47bee87261
Jacob Molland <jacobmolland28@gmail.com>
2026-04-13 23:37:46 -0400

    style: terraform fmt

--- baseline/envs/dev/backend.tf
+++ baseline/envs/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "tf-secure-baseline-state"
-    key     = "tf-state-baseline-dev"
-    region  = "us-east-1"
-    encrypt = true
+    bucket         = "tf-secure-baseline-state"
+    key            = "tf-state-baseline-dev"
+    region         = "us-east-1"
+    encrypt        = true
     dynamodb_table = "tf-secure-baseline-tf-lock"
   }
 }
--- baseline/envs/staging/backend.tf
+++ baseline/envs/staging/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "tf-secure-baseline-state"
-    key     = "tf-state-baseline-staging"
-    region  = "us-east-1"
-    encrypt = true
+    bucket         = "tf-secure-baseline-state"
+    key            = "tf-state-baseline-staging"
+    region         = "us-east-1"
+    encrypt        = true
     dynamodb_table = "tf-secure-baseline-tf-lock"
   }
 }
