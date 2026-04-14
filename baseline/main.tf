a5cf4677bd29f024778de99b4c38571990ba7997
Jacob Molland <jacobmolland28@gmail.com>
2026-04-14 17:42:27 -0400

    chore: pass var.environment into the 'identity_center' module

--- baseline/main.tf
+++ baseline/main.tf
@@ -253,6 +253,7 @@ module "identity_center" {
   logs_cmk_decrypt_policy_name = module.iam.logs_cmk_decrypt_policy_name
   logs_s3_readonly_policy_name = module.iam.logs_s3_readonly_policy_name
   secops_event_bus_arn         = module.automation.secops_event_bus_arn
+  environment = var.environment
   customer_managed_policy_path = "/"
 
   depends_on = [
