2162016ff43a4a1f9084663facb26f6ec7b51507
Jacob Molland <jacobmolland28@gmail.com>
2026-04-14 17:57:31 -0400

    style: terraform fmt

--- baseline/main.tf
+++ baseline/main.tf
@@ -143,7 +143,7 @@ module "monitoring" {
   name_prefix                         = local.name_prefix
   environment                         = var.environment
   logs_cmk_arn                        = module.security.logs_cmk_arn
-  cloudtrail_logs_group_name           = module.logging.cloudtrail_logs_group_name
+  cloudtrail_logs_group_name          = module.logging.cloudtrail_logs_group_name
   secops_emails                       = var.secops_emails
   tamper_detection_rule_arn           = module.security.tamper_detection_rule_arn
   account_id                          = data.aws_caller_identity.current.account_id
@@ -253,7 +253,7 @@ module "identity_center" {
   logs_cmk_decrypt_policy_name = module.iam.logs_cmk_decrypt_policy_name
   logs_s3_readonly_policy_name = module.iam.logs_s3_readonly_policy_name
   secops_event_bus_arn         = module.automation.secops_event_bus_arn
-  environment = var.environment
+  environment                  = var.environment
   customer_managed_policy_path = "/"
 
   depends_on = [
