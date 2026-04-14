96e540c686e73dd43bfe6dc2c9718c9e2f0bafd2
Jacob Molland <jacobmolland28@gmail.com>
2026-04-14 01:05:59 -0400

    fix: update name of dynamodb_table used by prod's backend

--- baseline/envs/prod/backend.tf
+++ baseline/envs/prod/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     bucket         = "tf-secure-baseline-state"
-    key            = "tf-state-baseline-prod"
+    key            = "tf-state-baseline-dev"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "tf-secure-baseline-tf-lock"
+    dynamodb_table = "tf-secure-baseline-lock"
   }
 }
