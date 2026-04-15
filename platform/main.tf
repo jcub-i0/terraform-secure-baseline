dcc04e641400257a28135ed888fb874d4fa6f61c
Jacob Molland <jacobmolland28@gmail.com>
2026-04-15 18:38:53 -0400

    feat: rename IAM Access Analyzer to include var.cloud_name instead of var.name_prefix; 'env' is included in 'name_prefix', which is pointless since this is an account-scoped resource

--- platform/main.tf
+++ platform/main.tf
@@ -11,7 +11,7 @@ resource "aws_iam_service_linked_role" "config" {
 
 ## IAM ACCESS ANALYZER
 resource "aws_accessanalyzer_analyzer" "main" {
-  analyzer_name = "${var.name_prefix}-account-access-analyzer"
+  analyzer_name = "${var.cloud_name}-account-access-analyzer"
   type          = "ACCOUNT"
 
   tags = {
