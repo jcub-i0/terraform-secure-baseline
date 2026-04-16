a6ce7b9008d4ad33cc475db9735d48184262aad2
Jacob Molland <jacobmolland28@gmail.com>
2026-04-16 00:54:48 -0400

    chore: remove baseline/backend.tf file

--- baseline/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tf-secure-baseline-state"
-    key            = "tf-state-baseline"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "tf-secure-baseline-lock"
-  }
-}
