17674b612d5381fe2099bdae1ee5638932bf32e0
Jacob Molland <jacobmolland28@gmail.com>
2026-04-17 17:59:27 -0400

    refactor: change name of backend 'key' for prod's 'account' substack

--- bootstrap/prod/account/backend.tf
+++ bootstrap/prod/account/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tf-secure-baseline-prod-state"
-    key            = "tf-state-bootstrap"
+    key            = "tf-state-bootstrap-prod"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "tf-secure-baseline-prod-lock"
