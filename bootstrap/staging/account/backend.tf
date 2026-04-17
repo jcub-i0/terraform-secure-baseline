f64dda9a4d66598e59910f3f092fe44098f43bea
Jacob Molland <jacobmolland28@gmail.com>
2026-04-17 17:59:28 -0400

    refactor: change name of backend 'key' for staging's 'account' substack

--- bootstrap/staging/account/backend.tf
+++ bootstrap/staging/account/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tf-secure-baseline-staging-state"
-    key            = "tf-state-bootstrap"
+    key            = "tf-state-bootstrap-staging"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "tf-secure-baseline-staging-lock"
