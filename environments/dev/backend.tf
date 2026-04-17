6f60aa2050c05a3ea2eead0d58ae96855567eec0
Jacob Molland <jacobmolland28@gmail.com>
2026-04-17 18:21:07 -0400

    refactor: update dev's 'baseline' stack to use the new s3 backend bucket/key and added 'use_lockfile' attribute, set to 'true'

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-secure-baseline-state"
+    bucket         = "tf-secure-baseline-dev-state"
     key            = "tf-state-baseline-dev"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "tf-secure-baseline-lock"
+    dynamodb_table = "tf-secure-baseline-dev-lock"
+    use_lockfile   = true
   }
 }
