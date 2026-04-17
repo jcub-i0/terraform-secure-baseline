d133d5b1d4d7299e9bf3cd37709752fb76c1d8d6
Jacob Molland <jacobmolland28@gmail.com>
2026-04-17 19:10:52 -0400

    refactor: in prod's bootstrap account stack, remove default values from cloud_name, environment, and primary_region vars

--- bootstrap/prod/account/variables.tf
+++ bootstrap/prod/account/variables.tf
@@ -5,18 +5,15 @@
 variable "cloud_name" {
   description = "The name of this cloud environment"
   type        = string
-  default     = "tf-secure-baseline"
 }
 
 variable "environment" {
   description = "Environment name"
   type        = string
-  default     = "prod"
 }
 
 variable "primary_region" {
   type    = string
-  default = "us-east-1"
 }
 
 variable "enable_github_oidc" {
