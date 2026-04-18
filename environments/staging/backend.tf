407c0d18775b16edfda254e865afca73288b3d67
Jacob Molland <jacobmolland28@gmail.com>
2026-04-18 02:53:53 -0400

    fix: modify staging backend's key to reference baseline vs bootstrap

--- environments/staging/backend.tf
+++ environments/staging/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tf-secure-baseline-staging-state"
-    key            = "tf-state-bootstrap-staging"
+    key            = "tf-state-baseline-staging"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "tf-secure-baseline-staging-lock"
