29d74b6d64d1ce1be204e5a0f4657865365aa484
Jacob Molland <jacobmolland28@gmail.com>
2026-04-17 22:48:07 -0400

    fix: in dev's account bootstrap stack, wrap outputs in a 'try()' function

--- bootstrap/dev/account/outputs.tf
+++ bootstrap/dev/account/outputs.tf
@@ -1,9 +1,9 @@
 output "github_plan_role_arn" {
   description = "ARN of the GitHub OIDC Terraform plan role"
-  value       = module.github_oidc[0].github_plan_role_arn
+  value       = try(module.github_oidc[0].github_plan_role_arn, null)
 }
 
 output "github_apply_role_arn" {
   description = "ARN of the GitHub OIDC Terraform apply role"
-  value       = module.github_oidc[0].github_apply_role_arn
+  value       = try(module.github_oidc[0].github_apply_role_arn, null)
 }
