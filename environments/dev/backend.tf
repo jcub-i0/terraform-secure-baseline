30d022d5f31d79bea614054f65d68cbfb7b67c68
Jacob Molland <jacobmolland28@gmail.com>
2026-04-21 00:50:13 -0400

    refactor: refactor dev's baseline s3 backend to use 'baseline/dev.tfstate' key

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tf-secure-baseline-dev-state"
-    key            = "tf-state-baseline-dev"
+    key            = "baseline/dev.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "tf-secure-baseline-dev-lock"
