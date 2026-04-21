6289f203b3a1e37cb5243e18b73d04bd0594cbb7
Jacob Molland <jacobmolland28@gmail.com>
2026-04-21 00:46:15 -0400

    refactor: rename prod's bootstrap s3 backend to use the 'bootstrap/prod.tfstate' key

--- bootstrap/prod/account/backend.tf
+++ bootstrap/prod/account/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tf-secure-baseline-prod-state"
-    key            = "tf-state-bootstrap-prod"
+    key            = "bootstrap/prod.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "tf-secure-baseline-prod-lock"
