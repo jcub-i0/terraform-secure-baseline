37de5f9be8d844d4c2a2e137c83772a605db79b6
Jacob Molland <jacobmolland28@gmail.com>
2026-04-22 18:19:46 -0400

    chore: comment out everything except for secops_operator-related variables in the 'identity_center' module

--- modules/identity_center/variables.tf
+++ modules/identity_center/variables.tf
@@ -3,16 +3,6 @@ variable "environment" {
   type        = string
 }
 
-variable "secops_analyst_group_name" {
-  description = "Name of the SecOps-Analyst IAM group"
-  type        = string
-}
-
-variable "secops_engineer_group_name" {
-  description = "Name of the SecOps-Engineer IAM group"
-  type        = string
-}
-
 variable "secops_operator_group_name" {
   description = "Name of the SecOps-Operator IAM group"
   type        = string
@@ -20,6 +10,21 @@ variable "secops_operator_group_name" {
 
 variable "account_id" {
   description = "ID of the AWS account managing this environment"
+  type        = string
+}
+
+variable "secops_event_bus_arn" {
+  description = "ARN of the SecOps Event Bus"
+  type        = string
+}
+/*
+variable "secops_analyst_group_name" {
+  description = "Name of the SecOps-Analyst IAM group"
+  type        = string
+}
+
+variable "secops_engineer_group_name" {
+  description = "Name of the SecOps-Engineer IAM group"
   type        = string
 }
 
@@ -38,8 +43,4 @@ variable "logs_cmk_decrypt_policy_name" {
   description = "'Name' attribute of the logs_cmk_decrypt policy"
   type        = string
 }
-
-variable "secops_event_bus_arn" {
-  description = "ARN of the SecOps Event Bus"
-  type        = string
-}
+*/
