d32446edaa889d9f5d4a4cd7e33f32dd4fdc10de
Jacob Molland <jacobmolland28@gmail.com>
2026-04-22 17:17:35 -0400

    chore: copy variables from 'identity_center' module into bootstrap/control_plane/identity_center/variables.tf

---
+++ bootstrap/control_plane/identity_center/variables.tf
@@ -0,0 +1,45 @@
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "secops_analyst_group_name" {
+  description = "Name of the SecOps-Analyst IAM group"
+  type        = string
+}
+
+variable "secops_engineer_group_name" {
+  description = "Name of the SecOps-Engineer IAM group"
+  type        = string
+}
+
+variable "secops_operator_group_name" {
+  description = "Name of the SecOps-Operator IAM group"
+  type        = string
+}
+
+variable "account_id" {
+  description = "ID of the AWS account managing this environment"
+  type        = string
+}
+
+variable "customer_managed_policy_path" {
+  description = "Path of customer managed IAM policies used by permission sets"
+  type        = string
+  default     = "/"
+}
+
+variable "logs_s3_readonly_policy_name" {
+  description = "'Name' attribute of the logs_s3_readonly policy"
+  type        = string
+}
+
+variable "logs_cmk_decrypt_policy_name" {
+  description = "'Name' attribute of the logs_cmk_decrypt policy"
+  type        = string
+}
+
+variable "secops_event_bus_arn" {
+  description = "ARN of the SecOps Event Bus"
+  type        = string
+}
