57a0d5a2c814a6bedfc7e854319a35c832640f83
Jacob Molland <jacobmolland28@gmail.com>
2026-04-22 19:23:26 -0400

    style: terraform fmt

--- bootstrap/control_plane/organizations/main.tf
+++ bootstrap/control_plane/organizations/main.tf
@@ -2,20 +2,20 @@ resource "aws_organizations_organization" "main" {
   feature_set = "ALL"
 
   aws_service_access_principals = []
-  enabled_policy_types = []
+  enabled_policy_types          = []
 }
 
 resource "aws_organizations_organizational_unit" "workloads" {
-  name = "Workloads"
+  name      = "Workloads"
   parent_id = aws_organizations_organization.main.roots[0].id
 }
 
 resource "aws_organizations_organizational_unit" "nonprod" {
-  name = "NonProd"
+  name      = "NonProd"
   parent_id = aws_organizations_organizational_unit.workloads.id
 }
 
 resource "aws_organizations_organizational_unit" "prod" {
-  name = "Prod"
+  name      = "Prod"
   parent_id = aws_organizations_organizational_unit.workloads.id
 }
