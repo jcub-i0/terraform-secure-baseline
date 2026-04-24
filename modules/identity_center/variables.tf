8ee5eafa8654bac4261f30c60708341e52f5488c
Jacob Molland <jacobmolland28@gmail.com>
2026-04-24 17:12:52 -0400

    chore: add validation for the 'logs_s3_readonly_policy_name' var in the 'identity_center' module's variables.tf file

--- modules/identity_center/variables.tf
+++ modules/identity_center/variables.tf
@@ -52,6 +52,14 @@ variable "logs_s3_readonly_policy_name" {
   description = "'Name' attribute of the logs_s3_readonly policy (set after 'baseline' is deployed)"
   type        = string
   default     = null
+
+  validation {
+    condition = !(
+      (var.enable_secops_analyst || var.enable_secops_engineer)
+      && var.logs_s3_readonly_policy_name == null
+    )
+    error_message = "'logs_s3_readonly_policy_name' must be set when 'enable_secops_analyst' or 'enable_secops_engineer' is set to 'true'"
+  }
 }
 
 variable "logs_cmk_decrypt_policy_name" {
