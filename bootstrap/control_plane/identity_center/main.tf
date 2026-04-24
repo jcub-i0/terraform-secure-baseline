bd9b87f832a13296e50196711548e4e4febd717a
Jacob Molland <jacobmolland28@gmail.com>
2026-04-24 18:58:30 -0400

    refactor: remove commented-out original 'identity_center' module call at the bottom of 'bootstrap/control_plane/identity_center/main.tf

--- bootstrap/control_plane/identity_center/main.tf
+++ bootstrap/control_plane/identity_center/main.tf
@@ -35,24 +35,3 @@ module "identity_center_staging" {
   secops_operator_group_name = "SecOps-Operator-Staging"
   secops_event_bus_arn       = "arn:aws:events:${var.staging_primary_region}:${var.staging_account_id}:event-bus/secops-bus"
 }
-
-/*
-# FULL IDENTITY_CENTER MODULE CALL
-module "identity_center" {
-  source = "../modules/identity_center"
-
-  account_id                   = data.aws_caller_identity.current.account_id
-  secops_analyst_group_name    = "SecOps-Analysts"
-  secops_engineer_group_name   = "SecOps-Engineers"
-  secops_operator_group_name   = "SecOps-Operators"
-  logs_cmk_decrypt_policy_name = module.iam.logs_cmk_decrypt_policy_name
-  logs_s3_readonly_policy_name = module.iam.logs_s3_readonly_policy_name
-  secops_event_bus_arn         = module.automation.secops_event_bus_arn
-  environment                  = var.environment
-  customer_managed_policy_path = "/"
-
-  depends_on = [
-    module.iam
-  ]
-}
-*/
