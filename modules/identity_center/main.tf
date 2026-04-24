28540d61356d07dd01d41f20c0941dda2af0c8dc
Jacob Molland <jacobmolland28@gmail.com>
2026-04-24 18:21:45 -0400

    chore: replace every instance of 'aws_ssoadmin_permission_set.secops_analyst.arn' with 'aws_ssoadmin_permission_set.secops_analyst[0].arn; the index is needed because 'count' is used throughout these resources

--- modules/identity_center/main.tf
+++ modules/identity_center/main.tf
@@ -76,7 +76,7 @@ resource "aws_ssoadmin_managed_policy_attachment" "secops_analyst_security_audit
   count = var.enable_secops_analyst ? 1 : 0
 
   instance_arn       = local.instance_arn
-  permission_set_arn = aws_ssoadmin_permission_set.secops_analyst.arn
+  permission_set_arn = aws_ssoadmin_permission_set.secops_analyst[0].arn
   managed_policy_arn = "arn:aws:iam::aws:policy/SecurityAudit"
 }
 
@@ -84,7 +84,7 @@ resource "aws_ssoadmin_managed_policy_attachment" "secops_analyst_readonly" {
   count = var.enable_secops_analyst ? 1 : 0
 
   instance_arn       = local.instance_arn
-  permission_set_arn = aws_ssoadmin_permission_set.secops_analyst.arn
+  permission_set_arn = aws_ssoadmin_permission_set.secops_analyst[0].arn
   managed_policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
 }
 
@@ -116,7 +116,7 @@ resource "aws_ssoadmin_customer_managed_policy_attachment" "secops_analyst_logs_
   count = var.logs_s3_readonly_policy_name ? 1 : 0
 
   instance_arn       = local.instance_arn
-  permission_set_arn = aws_ssoadmin_permission_set.secops_analyst.arn
+  permission_set_arn = aws_ssoadmin_permission_set.secops_analyst[0].arn
 
   customer_managed_policy_reference {
     name = var.logs_s3_readonly_policy_name
@@ -128,7 +128,7 @@ resource "aws_ssoadmin_customer_managed_policy_attachment" "secops_analyst_logs_
   count = var.logs_cmk_decrypt_policy_name ? 1 : 0
 
   instance_arn       = local.instance_arn
-  permission_set_arn = aws_ssoadmin_permission_set.secops_analyst.arn
+  permission_set_arn = aws_ssoadmin_permission_set.secops_analyst[0].arn
 
   customer_managed_policy_reference {
     name = var.logs_cmk_decrypt_policy_name
@@ -232,7 +232,7 @@ resource "aws_ssoadmin_account_assignment" "analysts" {
   count = var.enable_secops_analyst ? 1 : 0
 
   instance_arn       = local.instance_arn
-  permission_set_arn = aws_ssoadmin_permission_set.secops_analyst.arn
+  permission_set_arn = aws_ssoadmin_permission_set.secops_analyst[0].arn
 
   principal_id   = aws_identitystore_group.secops_analyst.group_id
   principal_type = "GROUP"
