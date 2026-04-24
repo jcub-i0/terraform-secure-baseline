819041f1d732f820d441d472112b83c8b077d40e
Jacob Molland <jacobmolland28@gmail.com>
2026-04-24 19:00:24 -0400

    style: terraform fmt

--- bootstrap/control_plane/identity_center/main.tf
+++ bootstrap/control_plane/identity_center/main.tf
@@ -21,10 +21,10 @@ module "identity_center_dev" {
 module "identity_center_prod" {
   source = "../../../modules/identity_center"
 
-  account_id                 = var.prod_account_id
-  environment                = "prod"
-  secops_operator_group_name = "SecOps-Operator-Prod"
-  secops_event_bus_arn       = "arn:aws:events:${var.prod_primary_region}:${var.prod_account_id}:event-bus/secops-bus"
+  account_id                   = var.prod_account_id
+  environment                  = "prod"
+  secops_operator_group_name   = "SecOps-Operator-Prod"
+  secops_event_bus_arn         = "arn:aws:events:${var.prod_primary_region}:${var.prod_account_id}:event-bus/secops-bus"
   enable_secops_analyst        = var.enable_secops_analyst
   enable_secops_engineer       = var.enable_secops_engineer
   logs_cmk_decrypt_policy_name = var.prod_logs_cmk_decrypt_policy_name
@@ -35,10 +35,10 @@ module "identity_center_prod" {
 module "identity_center_staging" {
   source = "../../../modules/identity_center"
 
-  account_id                 = var.staging_account_id
-  environment                = "staging"
-  secops_operator_group_name = "SecOps-Operator-Staging"
-  secops_event_bus_arn       = "arn:aws:events:${var.staging_primary_region}:${var.staging_account_id}:event-bus/secops-bus"
+  account_id                   = var.staging_account_id
+  environment                  = "staging"
+  secops_operator_group_name   = "SecOps-Operator-Staging"
+  secops_event_bus_arn         = "arn:aws:events:${var.staging_primary_region}:${var.staging_account_id}:event-bus/secops-bus"
   enable_secops_analyst        = var.enable_secops_analyst
   enable_secops_engineer       = var.enable_secops_engineer
   logs_cmk_decrypt_policy_name = var.staging_logs_cmk_decrypt_policy_name
