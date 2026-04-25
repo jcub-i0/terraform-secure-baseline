140bfecb388be34853aeb1ec481cce2e14fa0b58
Jacob Molland <jacobmolland28@gmail.com>
2026-04-24 22:47:36 -0400

    chore: change backend.tf for control_plane

--- bootstrap/control_plane/account/backend.tf
+++ bootstrap/control_plane/account/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-secure-baseline-dev-state"
-    key            = "bootstrap/dev.tfstate"
+    bucket         = "tf-secure-baseline-bootstrap-state"
+    key            = "control-plane/bootstrap.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "tf-secure-baseline-dev-lock"
+    dynamodb_table = "tf-secure-baseline-bootstrap-lock"
     use_lockfile   = true
   }
 }
