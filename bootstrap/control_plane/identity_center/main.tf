b20da05e9dc4288e18d55518a79dff21c5596496
Jacob Molland <jacobmolland28@gmail.com>
2026-04-24 20:46:56 -0400

    chore: remove data call for account_id from control_plane/identity_center/main.tf

--- bootstrap/control_plane/identity_center/main.tf
+++ bootstrap/control_plane/identity_center/main.tf
@@ -2,8 +2,6 @@ locals {
   name_prefix = "${var.cloud_name}-${var.environment}"
 }
 
-data "aws_caller_identity" "account_id" {}
-
 module "identity_center_dev" {
   source = "../../../modules/identity_center"
 
