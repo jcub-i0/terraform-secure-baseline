a567df78fee333c65413dea1d60ec6177e0e11d3
Jacob Molland <jacobmolland28@gmail.com>
2026-04-24 22:46:46 -0400

    chore: copy 'bootstrap/dev/account' stack to 'bootstrap/control_plane/'

---
+++ bootstrap/control_plane/account/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "tf-secure-baseline-dev-state"
+    key            = "bootstrap/dev.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "tf-secure-baseline-dev-lock"
+    use_lockfile   = true
+  }
+}
