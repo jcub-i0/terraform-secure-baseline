29f44098c313f549d15796fabca08cf72a44aacb
Jacob Molland <jacobmolland28@gmail.com>
2026-04-24 21:17:38 -0400

    style: terraform fmt

--- bootstrap/control_plane/identity_center/outputs.tf
+++ bootstrap/control_plane/identity_center/outputs.tf
@@ -1,14 +1,14 @@
 output "dev_permission_set_arns" {
   description = "Permission set ARNs for the 'dev' environment"
-  value = module.identity_center_dev.permission_set_arns
+  value       = module.identity_center_dev.permission_set_arns
 }
 
 output "prod_permission_set_arns" {
   description = "Permission set ARNs for the 'prod' environment"
-  value = module.identity_center_prod.permission_set_arns
+  value       = module.identity_center_prod.permission_set_arns
 }
 
 output "staging_permission_set_arns" {
   description = "Permission set ARNs for the 'staging' environment"
-  value = module.identity_center_staging.permission_set_arns
+  value       = module.identity_center_staging.permission_set_arns
 }
