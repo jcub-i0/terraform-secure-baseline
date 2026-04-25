dd723ca282f5a8a914bbdd67f90e4b1fe69a4c37
Jacob Molland <jacobmolland28@gmail.com>
2026-04-24 22:22:40 -0400

    style: terraform fmt

--- modules/identity_center/outputs.tf
+++ modules/identity_center/outputs.tf
@@ -4,11 +4,11 @@ output "permission_set_arns" {
     {
       secops-operator = aws_ssoadmin_permission_set.secops_operator.arn
     },
-    
+
     var.enable_secops_analyst ? {
-      secops-analyst  = aws_ssoadmin_permission_set.secops_analyst.arn
+      secops-analyst = aws_ssoadmin_permission_set.secops_analyst.arn
     } : {},
-    
+
     var.enable_secops_engineer ? {
       secops-engineer = aws_ssoadmin_permission_set.secops_engineer.arn
     } : {}
