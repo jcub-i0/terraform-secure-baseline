94df5554e53b3f518795305f814bb65d01036eb7
Jacob Molland <jacobmolland28@gmail.com>
2026-04-24 21:17:02 -0400

    chore: pass 'secops_analyst_<env>' and 'secops_engineer_<env>' variables for all environments' identity_center module calls

--- bootstrap/control_plane/identity_center/main.tf
+++ bootstrap/control_plane/identity_center/main.tf
@@ -10,7 +10,9 @@ module "identity_center_dev" {
   secops_operator_group_name   = "SecOps-Operator-Dev"
   secops_event_bus_arn         = "arn:aws:events:${var.primary_region_dev}:${var.account_id_dev}:event-bus/secops-bus"
   enable_secops_analyst        = var.enable_secops_analyst_dev
+  secops_analyst_group_name = "SecOps-Analyst-Dev"
   enable_secops_engineer       = var.enable_secops_engineer_dev
+  secops_engineer_group_name = "SecOps-Engineer-Dev"
   logs_cmk_decrypt_policy_name = var.logs_cmk_decrypt_policy_name_dev
   logs_s3_readonly_policy_name = var.logs_s3_readonly_policy_name_dev
   customer_managed_policy_path = "/"
@@ -24,7 +26,9 @@ module "identity_center_prod" {
   secops_operator_group_name   = "SecOps-Operator-Prod"
   secops_event_bus_arn         = "arn:aws:events:${var.primary_region_prod}:${var.account_id_prod}:event-bus/secops-bus"
   enable_secops_analyst        = var.enable_secops_analyst_prod
+  secops_analyst_group_name = "SecOps-Analyst-Prod"
   enable_secops_engineer       = var.enable_secops_engineer_prod
+  secops_engineer_group_name = "SecOps-Engineer-Prod"
   logs_cmk_decrypt_policy_name = var.logs_cmk_decrypt_policy_name_prod
   logs_s3_readonly_policy_name = var.logs_s3_readonly_policy_name_prod
   customer_managed_policy_path = "/"
@@ -38,7 +42,9 @@ module "identity_center_staging" {
   secops_operator_group_name   = "SecOps-Operator-Staging"
   secops_event_bus_arn         = "arn:aws:events:${var.primary_region_staging}:${var.account_id_staging}:event-bus/secops-bus"
   enable_secops_analyst        = var.enable_secops_analyst_staging
+  secops_analyst_group_name = "SecOps-Analyst-Prod"
   enable_secops_engineer       = var.enable_secops_engineer_staging
+  secops_engineer_group_name = "SecOps-Engineer-Staging"
   logs_cmk_decrypt_policy_name = var.logs_cmk_decrypt_policy_name_staging
   logs_s3_readonly_policy_name = var.logs_s3_readonly_policy_name_staging
   customer_managed_policy_path = "/"
