077714b67c51dbee8bf5ce1ba9b1a0fb3c7a5c9e
Jacob Molland <jacobmolland28@gmail.com>
2026-04-26 23:01:32 -0400

    refactor: rename key used by bootstrap/control_plane/account/backend.tf

--- bootstrap/control_plane/account/backend.tf
+++ bootstrap/control_plane/account/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tf-secure-baseline-bootstrap-state"
-    key            = "control-plane/bootstrap.tfstate"
+    key            = "control-plane/account.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "tf-secure-baseline-bootstrap-lock"
