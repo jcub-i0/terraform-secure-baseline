042acb016c75c9581157901990d80ac2291d8225
Jacob Molland <jacobmolland28@gmail.com>
2026-04-28 00:27:43 -0400

    refactor: remove '_staging' suffix from staging env's outputs

--- environments/staging/outputs.tf
+++ environments/staging/outputs.tf
@@ -1,19 +1,19 @@
-output "lambda_cmk_arn_staging" {
+output "lambda_cmk_arn" {
   description = "ARN of the 'staging' env's CMK used to encrypt Lambda functions"
   value       = module.baseline.lambda_cmk_arn
 }
 
-output "secrets_manager_cmk_arn_staging" {
+output "secrets_manager_cmk_arn" {
   description = "ARN of the 'staging' env's CMK used to encrypt Secrets Manager secrets"
   value       = module.baseline.secrets_manager_cmk_arn
 }
 
-output "logs_cmk_decrypt_policy_name_staging" {
+output "logs_cmk_decrypt_policy_name" {
   description = "'Name' attribute of the 'staging' env's 'Logs CMK Decrypt Policy' resource"
   value       = module.baseline.logs_cmk_decrypt_policy_name
 }
 
-output "logs_s3_readonly_policy_name_staging" {
+output "logs_s3_readonly_policy_name" {
   description = "'Name' attribute of the 'staging' env's 'Logs S3 Readonly Policy' resource"
   value       = module.baseline.logs_s3_readonly_policy_name
 }
