76986575c5ff7a61e88a5ace308a5084d59a5449
Jacob Molland <jacobmolland28@gmail.com>
2026-04-28 00:27:41 -0400

    refactor: remove '_dev' suffix from dev env's outputs

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -1,19 +1,19 @@
-output "lambda_cmk_arn_dev" {
+output "lambda_cmk_arn" {
   description = "ARN of the 'dev' env's CMK used to encrypt Lambda functions"
   value       = module.baseline.lambda_cmk_arn
 }
 
-output "secrets_manager_cmk_arn_dev" {
+output "secrets_manager_cmk_arn" {
   description = "ARN of the 'dev' env's CMK used to encrypt Secrets Manager secrets"
   value       = module.baseline.secrets_manager_cmk_arn
 }
 
-output "logs_cmk_decrypt_policy_name_dev" {
+output "logs_cmk_decrypt_policy_name" {
   description = "'Name' attribute of the 'dev' env's 'Logs CMK Decrypt Policy' resource"
   value       = module.baseline.logs_cmk_decrypt_policy_name
 }
 
-output "logs_s3_readonly_policy_name_dev" {
+output "logs_s3_readonly_policy_name" {
   description = "'Name' attribute of the 'dev' env's 'Logs S3 Readonly Policy' resource"
   value       = module.baseline.logs_s3_readonly_policy_name
 }
