f03c0b770dcc48ce09a4c4f7891c435ba3d367e1
Jacob Molland <jacobmolland28@gmail.com>
2026-04-29 19:33:34 -0400

    style: adjust spacing in github_oidc module call

--- bootstrap/dev/account/main.tf
+++ bootstrap/dev/account/main.tf
@@ -16,14 +16,17 @@ module "github_oidc" {
   branches_plan_github            = var.branches_plan_github
   allow_pull_requests_plan_github = var.allow_pull_requests_plan_github
   name_prefix                     = local.name_prefix
+
   tf_state_bucket_arn             = var.tf_state_bucket_arn
   tf_state_bucket_cmk_arn         = var.tf_state_bucket_cmk_arn
   tf_state_lock_table_arn         = var.tf_state_lock_table_arn
+
   primary_region                  = var.primary_region
   account_id                      = data.aws_caller_identity.current.account_id
   enable_apply_role_github        = var.enable_apply_role_github
   branches_apply_github           = var.branches_apply_github
   environment_apply_github        = var.environment_apply_github
+  
   lambda_cmk_arn                  = var.lambda_cmk_arn
   secrets_manager_cmk_arn         = var.secrets_manager_cmk_arn
 }
