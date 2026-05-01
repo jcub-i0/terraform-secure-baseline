d8bf3800488c67d393a90a81a2ac6ddf50543a47
Jacob Molland <jacobmolland28@gmail.com>
2026-05-01 14:44:30 -0400

    chore: remove 'dynamo_db_table' attribute from staging's backend for baseline

--- environments/staging/backend.tf
+++ environments/staging/backend.tf
@@ -4,7 +4,6 @@ terraform {
     key            = "baseline/staging.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "tf-secure-baseline-staging-lock"
     use_lockfile   = true
   }
 }
