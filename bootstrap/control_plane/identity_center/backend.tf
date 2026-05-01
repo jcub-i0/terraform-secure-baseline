d5129358aa472f57e0c29701c5380c9d1f084253
Jacob Molland <jacobmolland28@gmail.com>
2026-05-01 14:41:59 -0400

    refactor: remove 'dynamo_db_table' attribute from identity_center's backend

--- bootstrap/control_plane/identity_center/backend.tf
+++ bootstrap/control_plane/identity_center/backend.tf
@@ -4,7 +4,6 @@ terraform {
     key            = "control-plane/identity-center.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "tf-secure-baseline-control-plane-lock"
     use_lockfile   = true
   }
 }
