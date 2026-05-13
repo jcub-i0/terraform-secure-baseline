5f1533f0e39ca8814021249a1207e91f37386fe1
Jacob Molland <jacobmolland28@gmail.com>
2026-05-13 17:09:45 -0400

    chore: define 'egress_mode' variable in 'networking' module

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -25,3 +25,18 @@ variable "subnet_cidrs" {
 variable "firewall_endpoint_ids_by_az" {
   type = map(string)
 }
+
+variable "egress_mode" {
+  description = "Private subnet egress mode. Valid values: network_firewall, nat_only, vpc_endpoints_only"
+  type = string
+
+  validation {
+    condition = contains([
+      "network_firewall",
+      "nat_only",
+      "vpc_endpoints_only"
+    ], var.egress_mode)
+
+    error_message = "egress_mode must be one of: network_firewall, nat_only, vpc_endpoints_only."
+  }
+}
