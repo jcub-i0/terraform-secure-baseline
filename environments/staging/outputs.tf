be35b9c1cf161132ac74c8a03e75b861a46f7f6d
Jacob Molland <jacobmolland28@gmail.com>
2026-05-14 00:38:37 -0400

    refactor: update descriptions for outputs in each env

--- environments/staging/outputs.tf
+++ environments/staging/outputs.tf
@@ -39,21 +39,21 @@ output "effective_cloudwatch_retention_days" {
 }
 
 output "effective_enable_config" {
-  description = "Effective Enable Config value after resolving deployment_profile and enable_config"
+  description = "Effective AWS Config enablement after resolving deployment_profile and enable_config override"
   value = module.baseline.effective_enable_config
 }
 
 output "effective_enable_rules" {
-  description = "Effective Enable Rules value after resolving deployment_profile and enable_rules"
+  description = "Effective AWS Config rule group settings after resolving deployment_profile, enable_config, and enable_rules"
   value = module.baseline.effective_enable_rules
 }
 
 output "effective_backup_enabled" {
-  description = "Effective Backup Enabled value after resolving deployment_profile and backup_enabled"
+  description = "Effective AWS Backup enablement after resolving deployment_profile"
   value = module.baseline.effective_backup_enabled
 }
 
 output "effective_inspector_enabled" {
-  description = "Effective Inspector Enabled value after resolving deployment_profile and inspector_enabled"
+  description = "Effective Inspector enablement after resolving deployment_profile"
   value = module.baseline.effective_inspector_enabled
 }
