096a9bd371cce27808fd61ecb83ad64e6621cbcc
Jacob Molland <jacobmolland28@gmail.com>
2026-05-15 17:07:39 -0400

    style: terraform fmt

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -100,17 +100,17 @@ resource "aws_subnet" "firewall_private" {
 
 ## VPC ENDPOINT SUBNETS
 resource "aws_subnet" "endpoint_private" {
-  for_each = local.az_index_map
+  for_each                = local.az_index_map
   map_public_ip_on_launch = false
 
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.subnet_cidrs.endpoint_private[each.value]
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_cidrs.endpoint_private[each.value]
   availability_zone = each.key
 
   tags = {
-    Name = "${var.name_prefix}-Endpoint-Private-${each.key}"
+    Name        = "${var.name_prefix}-Endpoint-Private-${each.key}"
     Environment = var.environment
-    Terraform = "true"
+    Terraform   = "true"
   }
 }
 
