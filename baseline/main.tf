663fc4637fafef752221b728c6b8597fdce0acf5
Jacob Molland <jacobmolland28@gmail.com>
2026-05-15 20:21:18 -0400

    style: terraform fmt

--- baseline/main.tf
+++ baseline/main.tf
@@ -220,7 +220,7 @@ module "vpc_endpoints" {
   serverless_private_subnet_ids_map    = module.networking.serverless_private_subnet_ids_map
   endpoint_private_subnet_ids_map      = module.networking.endpoint_private_subnet_ids_map
   endpoint_private_route_table_ids_map = module.networking.endpoint_private_rt
-  
+
   s3_gateway_endpoint_rt_ids_list = concat(
     values(module.networking.endpoint_private_route_table_ids_map),
     values(module.networking.compute_private_route_table_ids_map),
