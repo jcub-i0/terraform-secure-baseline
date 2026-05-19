25ad4e9fd93d6c62928c8ec61b021cab5ea7f1fa
Jacob Molland <jacobmolland28@gmail.com>
2026-05-18 22:59:54 -0400

    fix: wrap 'identifiers' attribute in brackets

--- modules/iam/ec2.tf
+++ modules/iam/ec2.tf
@@ -9,7 +9,7 @@ data "aws_iam_policy_document" "ec2_assume_role" {
 
     principals {
       type        = "Service"
-      identifiers = "ec2.amazonaws.com"
+      identifiers = ["ec2.amazonaws.com"]
     }
   }
 }
