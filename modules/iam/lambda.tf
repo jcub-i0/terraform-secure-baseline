2206c81340293e2da08014abd81ff3924a63de57
Jacob Molland <jacobmolland28@gmail.com>
2026-05-18 23:05:48 -0400

    style: terraform fmt

--- modules/iam/lambda.tf
+++ modules/iam/lambda.tf
@@ -16,12 +16,12 @@ data "aws_iam_policy" "lambda_xray" {
 ### EC2 ISOLATION LAMBDA TRUST POLICY
 data "aws_iam_policy_document" "lambda_ec2_isolation_assume_role" {
   statement {
-    sid = "LambdaEC2IsolationAssumeRole"
-    effect = "Allow"
+    sid     = "LambdaEC2IsolationAssumeRole"
+    effect  = "Allow"
     actions = ["sts:AssumeRole"]
 
     principals {
-      type = "Service"
+      type        = "Service"
       identifiers = ["lambda.amazonaws.com"]
     }
   }
@@ -29,7 +29,7 @@ data "aws_iam_policy_document" "lambda_ec2_isolation_assume_role" {
 
 ### EC2 ISOLATION LAMBDA EXECUTION ROLE
 resource "aws_iam_role" "lambda_ec2_isolation" {
-  name = "${var.name_prefix}-lambda-ec2-isolation-role"
+  name               = "${var.name_prefix}-lambda-ec2-isolation-role"
   assume_role_policy = data.aws_iam_policy_document.lambda_ec2_isolation_assume_role.json
 }
 
