2aae41e5d5b90c13e64b1b3ddd574662b8101bb5
Jacob Molland <jacobmolland28@gmail.com>
2026-05-20 18:50:49 -0400

    refactor: reconfigure logs_s3_readonly iam policy to leverage iam policy document data block

--- modules/iam/shared_policies.tf
+++ modules/iam/shared_policies.tf
@@ -1,36 +1,36 @@
 # SHARED POLICIES ATTACHED TO MULTIPLE ROLES
-## GENERIC POLICY TO ALLOW READ ACCESS TO CENTRALIZED LOGS S3 BUCKET
+
+# GENERIC POLICY TO ALLOW READ ACCESS TO CENTRALIZED LOGS S3 BUCKET
+data "aws_iam_policy_document" "logs_s3_readonly" {
+  statement {
+    sid = "ListCentralizedLogsBucket"
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation"
+    ]
+
+    resources = [var.centralized_logs_bucket_arn]
+  }
+
+  statement {
+    sid = "ReadCentralizedLogsObjects"
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetObjectTagging",
+      "s3:GetObjectVersionTagging"
+    ]
+
+    resources = ["${var.centralized_logs_bucket_arn}/*"]
+  }
+}
+
 resource "aws_iam_policy" "logs_s3_readonly" {
   name        = "${var.name_prefix}-CentralizedLogsS3ReadOnly"
   description = "Read-only access to Centralized Logs S3 bucket (no delete, no write)"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      # LIST BUCKET + READ BUCKET METADATA
-      {
-        Sid    = "ListCentralizedLogsBucket"
-        Effect = "Allow"
-        Action = [
-          "s3:ListBucket",
-          "s3:GetBucketLocation"
-        ]
-        Resource = var.centralized_logs_bucket_arn
-      },
-      # READ OBJECTS + VERSIONS
-      {
-        Sid    = "ReadCentralizedLogsObjects"
-        Effect = "Allow"
-        Action = [
-          "s3:GetObject",
-          "s3:GetObjectVersion",
-          "s3:GetObjectTagging",
-          "s3:GetObjectVersionTagging"
-        ]
-        Resource = "${var.centralized_logs_bucket_arn}/*"
-      }
-    ]
-  })
+  policy = data.aws_iam_policy_document.logs_s3_readonly
 }
 
 ## GENERIC POLICY TO ALLOW DECRYPTION OF OBJECTS ENCRYPTED WITH THE LOGS CMK
