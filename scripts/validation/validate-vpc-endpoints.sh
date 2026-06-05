6e2b045f350869eba0918efefea2b6fb762268bd
Jacob Molland <jacobmolland28@gmail.com>
2026-06-05 13:46:37 -0400

    fix: remove extra quote and add necessary backslash in 'ENDPOINT_ROUTE_TABLES_JSON' var

--- scripts/validation/validate-vpc-endpoints.sh
+++ scripts/validation/validate-vpc-endpoints.sh
@@ -165,7 +165,7 @@ ENDPOINT_ROUTE_TABLES_JSON="$(
     "${aws_args[@]}" \
     --filters \
       "Name=vpc-id,Values=${VPC_ID}" \
-      "Name=tag:Name,Values="${NAME_PREFIX}-Endpoint-Private-RT-*"
+      "Name=tag:Name,Values=${NAME_PREFIX}-Endpoint-Private-RT-*" \
     --output json
 )"
 
