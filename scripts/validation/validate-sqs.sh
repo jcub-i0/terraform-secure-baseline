a0c09cad9953b67cd05292242015a432b86b44a6
Jacob Molland <jacobmolland28@gmail.com>
2026-06-12 12:58:46 -0400

    chore: draft up 'Checking required local commands' section

--- scripts/validation/validate-sqs.sh
+++ scripts/validation/validate-sqs.sh
@@ -58,3 +58,19 @@ if [[ -n "$AWS_REGION" ]]; then
   aws_args+=(--region "$AWS_REGION")
 fi
 
+section "tf-secure-baseline SQS Validation"
+
+section "Checking required local commands"
+
+require_command aws
+success "aws CLI found"
+
+require_command terraform
+success "terraform found"
+
+require_command jq
+success "jq found"
+
+require_command git
+success "git found"
+
