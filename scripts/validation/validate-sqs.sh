df398323eda9250835cc2a362b6e63ba1bbcff97
Jacob Molland <jacobmolland28@gmail.com>
2026-06-15 20:39:22 -0400

    fix: SCRIPT_DIR definition in validate-sqs.sh

--- scripts/validation/validate-sqs.sh
+++ scripts/validation/validate-sqs.sh
@@ -31,7 +31,7 @@
 
 set -euo pipefail
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[@]}")" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck source=lib/common.sh
 source "${SCRIPT_DIR}/lib/common.sh"
 
