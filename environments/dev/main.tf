dd62961439a133afc11aac8644abdf9250d7439f
Jacob Molland <jacobmolland28@gmail.com>
2026-06-19 15:27:14 -0400

    chore: pass 'inspector_enabled' var into baseline module call from dev env's main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -14,6 +14,7 @@ module "baseline" {
   enable_config                      = var.enable_config
   enable_rules                       = var.enable_rules
   backup_enabled                     = var.backup_enabled
+  inspector_enabled = var.inspector_enabled
   deployment_profile                 = var.deployment_profile
   egress_mode                        = var.egress_mode
   break_glass_trusted_principal_arns = var.break_glass_trusted_principal_arns
