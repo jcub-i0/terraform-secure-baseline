0d6c2aec0de55fda60ee979f3f7dce1a3e25fc93
Jacob Molland <jacobmolland28@gmail.com>
2026-06-19 17:11:01 -0400

    chore: tighten up 'AllowLambdaUse' kms policy statement for the 'lambda' CMK by adding a 'kms:ViaService' condition

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -411,6 +411,7 @@ resource "aws_kms_key" "lambda" {
         Condition = {
           StringEquals = {
             "aws:SourceAccount" = var.account_id
+            "kms:ViaService" = "lambda.${var.primary_region}.amazonaws.com"
           }
         }
       }
