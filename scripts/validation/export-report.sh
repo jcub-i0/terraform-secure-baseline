7a3fb34642efc778079e38765b0e844158a8d567
Jacob Molland <jacobmolland28@gmail.com>
2026-06-20 00:14:53 -0400

    chore: include 'ENV_NAME' var in markdown summary

--- scripts/validation/export-report.sh
+++ scripts/validation/export-report.sh
@@ -215,7 +215,7 @@ section "Generating Markdown summary"
 {
   echo "# tf-secure-baseline Validation Report"
   echo
-  echo "The `dev` workload environment passed automated read-only validation."
+  echo "The `${ENV_NAME}` workload environment passed automated read-only validation."
   echo
   echo "## Executive Summary"
   echo
