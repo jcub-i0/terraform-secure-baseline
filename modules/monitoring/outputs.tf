d636419ae6603ba5fb942a314a7a9293fde5cf63
Jacob Molland <jacobmolland28@gmail.com>
2026-06-26 17:06:01 -0400

    chore: define 'sec_notifs_eventbridge_dlq_arn' output from the  module

--- modules/monitoring/outputs.tf
+++ modules/monitoring/outputs.tf
@@ -5,3 +5,7 @@ output "compliance_topic_arn" {
 output "secops_topic_arn" {
   value = aws_sns_topic.secops.arn
 }
+
+output "sec_notifs_eventbridge_dlq_arn" {
+  value = aws_sqs_queue.security_notifications_eventbridge_dlq.arn
+}
