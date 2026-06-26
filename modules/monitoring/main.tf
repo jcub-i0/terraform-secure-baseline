2f2e6e79920d1dfb07b44214c73245de200b9cea
Jacob Molland <jacobmolland28@gmail.com>
2026-06-26 15:54:10 -0400

    feat: in  module, add 'security_notifications_eventbridge_dlq' SQS queue/DLQ

--- modules/monitoring/main.tf
+++ modules/monitoring/main.tf
@@ -418,6 +418,21 @@ resource "aws_sqs_queue_policy" "security_notifications" {
   policy    = data.aws_iam_policy_document.security_notifications_sqs.json
 }
 
+### SECURITY NOTIFICATIONS EVENTBRIDGE DLQ
+resource "aws_sqs_queue" "security_notifications_eventbridge_dlq" {
+  name = "${var.name_prefix}-security-notifications-eventbridge-dlq"
+  kms_master_key_id = var.logs_cmk_arn
+
+  # Maximum retention time for troubleshooting (14 days)
+  message_retention_seconds = 1209600
+
+  tags = {
+    Name = "${var.name_prefix}-Security-Notifications-EventBridge-DLQ"
+    Environment = var.environment
+    Terraform = "true"
+  }
+}
+
 ### CLOUDWATCH EVENT RULES
 
 ##########################################
