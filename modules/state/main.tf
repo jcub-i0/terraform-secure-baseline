a5e321417bc3a8700cd4f3497cd5f579e7f4e279
Jacob Molland <jacobmolland28@gmail.com>
2026-07-03 20:46:33 -0400

    chore: remove dynamodb state_lock resource from 'state' module

--- modules/state/main.tf
+++ modules/state/main.tf
@@ -194,29 +194,3 @@ resource "aws_s3_bucket_policy" "state" {
   bucket = aws_s3_bucket.state.id
   policy = data.aws_iam_policy_document.state_bucket.json
 }
-
-# STATE DYNAMODB LOCK TABLE
-resource "aws_dynamodb_table" "state_lock" {
-  name         = "${var.name_prefix}-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  point_in_time_recovery {
-    enabled = true
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  tags = {
-    Name        = "${var.name_prefix}-Terraform-Lock"
-    Environment = var.environment
-    Terraform   = "true"
-  }
-}
