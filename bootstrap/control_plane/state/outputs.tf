47e5f51cebc64799c09d290b73ee79d791faa602
Jacob Molland <jacobmolland28@gmail.com>
2026-07-03 20:53:13 -0400

    chore: remove state_lock output from bootstrap/state/outputs.tf

--- bootstrap/control_plane/state/outputs.tf
+++ bootstrap/control_plane/state/outputs.tf
@@ -17,13 +17,3 @@ output "tf_state_bucket_cmk_arn" {
   description = "ARN of the KMS CMK used to encrypt the Terraform State bucket"
   value       = module.state.tf_state_bucket_cmk_arn
 }
-
-output "tf_state_lock_table_arn" {
-  description = "ARN of the State Lock DynamoDB table used for Terraform state locking"
-  value       = module.state.tf_state_lock_table_arn
-}
-
-output "tf_state_lock_table_name" {
-  description = "value"
-  value       = module.state.tf_state_lock_table_name
-}
