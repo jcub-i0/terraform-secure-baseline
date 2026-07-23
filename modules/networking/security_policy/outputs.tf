9fe0f18d6d85133a79fbaadf4759afc3350f8216
Jacob Molland <jacobmolland28@gmail.com>
2026-07-22 21:00:30 -0400

    style: terraform fmt

--- modules/networking/security_policy/outputs.tf
+++ modules/networking/security_policy/outputs.tf
@@ -4,7 +4,7 @@ output "compute_policy_rule_ids" {
   value = {
     endpoints_ingress_from_compute = aws_security_group_rule.endpoints_ingress_from_compute.id
     compute_egress_to_endpoints    = aws_security_group_rule.compute_egress_to_endpoints.id
-    compute_egress_to_db = aws_security_group_rule.compute_egress_to_db.id
+    compute_egress_to_db           = aws_security_group_rule.compute_egress_to_db.id
 
     compute_egress_to_internet_https = try(
       aws_security_group_rule.compute_egress_to_internet_https[0].id,
