dba4601768010e3a77a6a1c252de3f2582776919
Jacob Molland <jacobmolland28@gmail.com>
2026-07-31 14:44:13 -0400

    chore: define outputs.tf in security_operations/state/ stack

---
+++ bootstrap/security_operations/state/outputs.tf
@@ -0,0 +1,19 @@
+output "control_plane_account_id" {
+  description = "ID of the AWS account managing this environment"
+  value       = data.aws_caller_identity.account_id.account_id
+}
+
+output "tf_state_bucket_arn" {
+  description = "ARN of the S3 bucket where the Terraform state is stored"
+  value       = module.state.tf_state_bucket_arn
+}
+
+output "tf_state_bucket_name" {
+  description = "Name of the S3 bucket where the Terraform state is stored"
+  value       = module.state.tf_state_bucket_name
+}
+
+output "tf_state_bucket_cmk_arn" {
+  description = "ARN of the KMS CMK used to encrypt the Terraform State bucket"
+  value       = module.state.tf_state_bucket_cmk_arn
+}
