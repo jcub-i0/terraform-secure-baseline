4ccfb11e7504da70517077d2d2905038dc62c073
Jacob Molland <jacobmolland28@gmail.com>
2026-07-31 23:07:12 -0400

    chore: define primary_region, account_id, and enable_securityhub_organization_configuration variables in security_operations/security_services

--- bootstrap/security_operations/security_services/variables.tf
+++ bootstrap/security_operations/security_services/variables.tf
@@ -1,13 +1,35 @@
 variable "cloud_name" {
-  description = "The name of this cloud environment"
+  description = "Name of the cloud platform"
   type        = string
 }
 
 variable "environment" {
-  description = "Environment name"
+  description = "Name of the specialized security account environment"
   type        = string
+
+  validation {
+    condition     = var.environment == "security-operations"
+    error_message = "environment must be security-operations."
+  }
 }
 
 variable "primary_region" {
-  type = string
+  description = "Primary and Security Hub home Region"
+  type        = string
+}
+
+variable "account_id" {
+  description = "AWS account ID of the security-operations account"
+  type        = string
+
+  validation {
+    condition     = can(regex("^[0-9]{12}$", var.account_id))
+    error_message = "account_id must be a 12-digit AWS account ID."
+  }
+}
+
+variable "enable_securityhub_organization_configuration" {
+  description = "Enable Security Hub central organization configuration after delegated administration is configured"
+  type        = bool
+  default     = false
 }
