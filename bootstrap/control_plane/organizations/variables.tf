e75e12d806a7c095b66acdb27027793628039252
Jacob Molland <jacobmolland28@gmail.com>
2026-08-01 13:44:26 -0400

    chore: define 'security_operations_account_id' var in organizations stack

---
+++ bootstrap/control_plane/organizations/variables.tf
@@ -0,0 +1,9 @@
+variable "security_operations_account_id" {
+  description = "AWS account ID designated as the centralized security-services administrator"
+  type = string
+
+  validation {
+    condition     = can(regex("^[0-9]{12}$", var.security_operations_account_id))
+    error_message = "security_operations_account_id must be a 12-digit AWS account ID."
+  }
+}
