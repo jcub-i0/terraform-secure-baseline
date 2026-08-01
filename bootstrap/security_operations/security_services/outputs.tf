5b87c54c3094fa235ff45e366df83e54a0648410
Jacob Molland <jacobmolland28@gmail.com>
2026-07-31 23:21:47 -0400

    chore: define 'security_operations_account_id', 'securityhub_home_region', 'securityhub_finding_aggregator_arn', and 'securityhub_central_configuration_enabled' outputs in security_services/outputs.tf

---
+++ bootstrap/security_operations/security_services/outputs.tf
@@ -0,0 +1,19 @@
+output "security_operations_account_id" {
+  description = "AWS account ID managing centralized security services"
+  value       = data.aws_caller_identity.current.account_id
+}
+
+output "securityhub_home_region" {
+  description = "Security Hub home Region"
+  value       = var.primary_region
+}
+
+output "securityhub_finding_aggregator_arn" {
+  description = "ARN of the Security Hub finding aggregator"
+  value       = aws_securityhub_finding_aggregator.this.arn
+}
+
+output "securityhub_central_configuration_enabled" {
+  description = "Whether Security Hub central organization configuration is enabled"
+  value       = var.enable_securityhub_organization_configuration
+}
