99c43cd39e4e4c80fa4598d8ef5f493c757ea8cd
Jacob Molland <jacobmolland28@gmail.com>
2026-08-01 13:50:42 -0400

    feat: integrate security hub into organizations

--- bootstrap/control_plane/organizations/main.tf
+++ bootstrap/control_plane/organizations/main.tf
@@ -29,3 +29,23 @@ resource "aws_organizations_organizational_unit" "security" {
   name      = "Security"
   parent_id = data.aws_organizations_organization.main.roots[0].id
 }
+
+##########################################
+# SECURITY HUB ORGANIZATION INTEGRATION
+##########################################
+
+resource "aws_organizations_aws_service_access" "securityhub" {
+  count = var.enable_securityhub_delegated_administrator ? 1 : 0
+
+  service_principal = "securityhub.amazonaws.com"
+}
+
+resource "aws_securityhub_organization_admin_account" "security_operations" {
+  count = var.enable_securityhub_delegated_administrator ? 1 : 0
+
+  admin_account_id = var.security_operations_account_id
+
+  depends_on = [
+    aws_organizations_aws_service_access.securityhub
+  ]
+}
