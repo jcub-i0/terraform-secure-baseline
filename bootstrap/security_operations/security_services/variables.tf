bbbd41230f9615c0a56597679bba592703c889fe
Jacob Molland <jacobmolland28@gmail.com>
2026-07-31 23:00:35 -0400

    chore: define 'cloud_name', 'environment', and 'primary_region' vars in variables.tf

---
+++ bootstrap/security_operations/security_services/variables.tf
@@ -0,0 +1,13 @@
+variable "cloud_name" {
+  description = "The name of this cloud environment"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "primary_region" {
+  type = string
+}
