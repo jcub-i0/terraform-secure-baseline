8a316df1c54336a7b6396bfceba4d8c46fbeb6de
Jacob Molland <jacobmolland28@gmail.com>
2026-08-01 15:27:12 -0400

    refactor: remove unused 'security_operations_account_id' var from organizations stack

--- bootstrap/control_plane/organizations/variables.tf
+++ bootstrap/control_plane/organizations/variables.tf
@@ -1,13 +1,3 @@
-variable "security_operations_account_id" {
-  description = "AWS account ID designated as the centralized security-services administrator"
-  type        = string
-
-  validation {
-    condition     = can(regex("^[0-9]{12}$", var.security_operations_account_id))
-    error_message = "security_operations_account_id must be a 12-digit AWS account ID."
-  }
-}
-
 variable "enable_securityhub_delegated_administrator" {
   description = "Enable Security Hub trusted accerss and designate the security-operations account as administrator"
   type        = bool
