52ce61413a14902ef3d5e0e0061e0ae77dd0a67a
Jacob Molland <jacobmolland28@gmail.com>
2026-08-03 22:50:18 -0400

    refactor: improve description of 'manage_securityhub_cspm_locally' var

--- baseline/variables.tf
+++ baseline/variables.tf
@@ -277,7 +277,7 @@ variable "isolation_allowed" {
 }
 
 variable "manage_securityhub_cspm_locally" {
-  description = "Manage Security Hub CSPM and standards in the workload account"
+  description = "Whether this workload account manages its own Security Hub CSPM enablement and standards. Set to false when CSPM is centrally managed from the security-operations account."
   type        = bool
-  default     = true
+  default     = false
 }
