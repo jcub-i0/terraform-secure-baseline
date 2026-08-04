7d69b7c6997036d91bceecfd523b49224c425596
Jacob Molland <jacobmolland28@gmail.com>
2026-08-03 23:12:50 -0400

    chore: set 'environment' var default to <env> for each workload env

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -6,6 +6,7 @@ variable "cloud_name" {
 variable "environment" {
   description = "Environment name"
   type        = string
+  default = "dev"
 }
 
 variable "deployment_profile" {
--- environments/staging/variables.tf
+++ environments/staging/variables.tf
@@ -6,6 +6,7 @@ variable "cloud_name" {
 variable "environment" {
   description = "Environment name"
   type        = string
+  default = "staging"
 }
 
 variable "deployment_profile" {
