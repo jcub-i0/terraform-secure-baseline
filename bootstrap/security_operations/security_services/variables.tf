66f6e28ef5fda51b0b9b205a1e8fcd91724761d4
Jacob Molland <jacobmolland28@gmail.com>
2026-08-03 21:43:46 -0400

    refactor: spacing

--- bootstrap/security_operations/security_services/variables.tf
+++ bootstrap/security_operations/security_services/variables.tf
@@ -46,7 +46,10 @@ variable "securityhub_cspm_account_policies" {
   type = map(object({
     create_policy                = optional(bool, false)
     associate_policy             = optional(bool, false)
-    enabled_standards            = optional(set(string), ["aws_fsbp", "cis_5_0"])
+    enabled_standards            = optional(set(string), [
+        "aws_fsbp",
+        "cis_5_0"
+    ])
     disabled_control_identifiers = optional(set(string), [])
   }))
 
