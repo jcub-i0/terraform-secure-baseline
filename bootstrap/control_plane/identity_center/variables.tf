99d87b252fa1cd108b0d3dd6f17244b0fc9f9de6
Jacob Molland <jacobmolland28@gmail.com>
2026-08-05 20:22:59 -0400

    chore: remove all vars in identity center stack that have been replaced by new 'identity_center_workloads' variable

--- bootstrap/control_plane/identity_center/variables.tf
+++ bootstrap/control_plane/identity_center/variables.tf
@@ -1,134 +1,3 @@
-variable "enable_secops_analyst_dev" {
-  description = "Determines whether SecOps-Analyst resources are deployed in the 'dev' env"
-  type        = bool
-  default     = false
-}
-
-variable "enable_secops_analyst_prod" {
-  description = "Determines whether SecOps-Analyst resources are deployed in the 'prod' env"
-  type        = bool
-  default     = false
-}
-
-variable "enable_secops_analyst_staging" {
-  description = "Determines whether SecOps-Analyst resources are deployed in the 'staging' env"
-  type        = bool
-  default     = false
-}
-
-variable "enable_secops_analyst_secops" {
-  description = "Determines whether SecOps-Analyst resources are deployed in the 'security-operations' env"
-  type        = bool
-  default     = false
-}
-
-variable "enable_secops_engineer_dev" {
-  description = "Determines whether SecOps-Engineer resources are deployed in the 'dev' env"
-  type        = bool
-  default     = false
-}
-
-variable "enable_secops_engineer_prod" {
-  description = "Determines whether SecOps-Engineer resources are deployed in the 'prod' env"
-  type        = bool
-  default     = false
-}
-
-variable "enable_secops_engineer_staging" {
-  description = "Determines whether SecOps-Engineer resources are deployed in the 'staging' env"
-  type        = bool
-  default     = false
-}
-
-variable "enable_secops_engineer_secops" {
-  description = "Determines whether SecOps-Engineer resources are deployed in the 'security-operations' env"
-  type        = bool
-  default     = false
-}
-
-variable "account_id_dev" {
-  description = "ID of the AWS account managing the 'dev' environment"
-  type        = string
-}
-
-variable "account_id_prod" {
-  description = "ID of the AWS account managing the 'prod' environment"
-  type        = string
-}
-
-variable "account_id_staging" {
-  description = "ID of the AWS account managing the 'staging' environment"
-  type        = string
-}
-
-variable "account_id_secops" {
-  description = "ID of the AWS account managing the 'security-operations' environment"
-  type        = string
-}
-
-variable "primary_region_dev" {
-  description = "Primary region used by the 'dev' environment"
-  type        = string
-}
-
-variable "primary_region_prod" {
-  type        = string
-  description = "Primary region used by the 'prod' environment"
-}
-
-variable "primary_region_staging" {
-  type        = string
-  description = "Primary region used by the 'staging' environment"
-}
-
-variable "logs_s3_readonly_policy_name_dev" {
-  description = "'Name' attribute of the 'dev' env's logs_s3_readonly policy"
-  type        = string
-  default     = null
-}
-
-variable "logs_s3_readonly_policy_name_prod" {
-  description = "'Name' attribute of the 'prod' env's logs_s3_readonly policy"
-  type        = string
-  default     = null
-}
-
-variable "logs_s3_readonly_policy_name_staging" {
-  description = "'Name' attribute of the 'staging' env's logs_s3_readonly policy"
-  type        = string
-  default     = null
-}
-
-variable "logs_s3_readonly_policy_name_secops" {
-  description = "'Name' attribute of the 'security-operations' env's logs_s3_readonly policy"
-  type        = string
-  default     = null
-}
-
-variable "logs_cmk_decrypt_policy_name_dev" {
-  description = "'Name' attribute of the 'dev' env's logs_cmk_decrypt policy"
-  type        = string
-  default     = null
-}
-
-variable "logs_cmk_decrypt_policy_name_prod" {
-  description = "'Name' attribute of the 'prod' env's logs_cmk_decrypt policy"
-  type        = string
-  default     = null
-}
-
-variable "logs_cmk_decrypt_policy_name_staging" {
-  description = "'Name' attribute of the 'staging' env's logs_cmk_decrypt policy"
-  type        = string
-  default     = null
-}
-
-variable "logs_cmk_decrypt_policy_name_secops" {
-  description = "'Name' attribute of the 'security-operations' env's logs_cmk_decrypt policy"
-  type        = string
-  default     = null
-}
-
 variable "identity_center_workloads" {
   description = "Identity Center configuraiton for workload accounts"
 
@@ -158,4 +27,21 @@ variable "identity_center_workloads" {
 
     error_message = "Each workload account ID must contain exactly 12 digits."
   }
+}
+
+variable "enable_secops_engineer_secops" {
+  description = "Determines whether SecOps-Engineer resources are deployed in the 'security-operations' env"
+  type        = bool
+  default     = false
+}
+
+variable "account_id_secops" {
+  description = "ID of the AWS account managing the 'security-operations' environment"
+  type        = string
+}
+
+variable "logs_cmk_decrypt_policy_name_secops" {
+  description = "'Name' attribute of the 'security-operations' env's logs_cmk_decrypt policy"
+  type        = string
+  default     = null
 }
