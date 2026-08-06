ae891dc86729efacaf08818378621a7c045de8fa
Jacob Molland <jacobmolland28@gmail.com>
2026-08-05 20:54:59 -0400

    chore: define 'workload_permission_set_arns' and 'secops_permission_set_arns' outputs

---
+++ bootstrap/control_plane/identity_center/outputs.tf
@@ -0,0 +1,13 @@
+output "workload_permission_set_arns" {
+  description = "Permission set ARNs by workload environment"
+
+  value = {
+    for environment, identity_center in module.identity_center_workloads :
+    environment => identity_center.permission_set_arns
+  }
+}
+
+output "secops_permission_set_arns" {
+  description = "Permission set ARNs for the security-operations account"
+  value = module.identity_center_secops.permission_set_arns
+}
