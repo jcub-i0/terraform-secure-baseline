b97da47090490b6e056e60dd6697f61e3305dc57
Jacob Molland <jacobmolland28@gmail.com>
2026-08-07 22:20:04 -0400

    chore: reconfigure outputs from security_services (will be consumed by validation script(s))

--- bootstrap/control_plane/organizations/outputs.tf
+++ bootstrap/control_plane/organizations/outputs.tf
@@ -1,9 +1,43 @@
-output "securityhub_delegated_administrator_account_id" {
-  description = "Account ID designated as the Security Hub administrator"
-  value = try(
-    aws_securityhub_organization_admin_account.security_operations[0].admin_account_id,
-    null
-  )
+output "organization_id" {
+  description = "AWS Organizations organization ID"
+  value       = aws_organizations_organization.main.id
+}
+
+output "organization_root_id" {
+  description = "AWS Organizations root ID"
+  value       = aws_organizations_organization.main.roots[0].id
+}
+
+output "organizational_unit_ids" {
+  description = "AWS Organizations organizational unit IDs managed by this stack"
+
+  value = {
+    workloads = aws_organizations_organizational_unit.workloads.id
+    nonprod   = aws_organizations_organizational_unit.nonprod.id
+    prod      = aws_organizations_organizational_unit.prod.id
+    security  = aws_organizations_organizational_unit.security.id
+  }
+}
+
+output "security_operations_account_id" {
+  description = "AWS account ID used for centralized security operations"
+  value       = local.security_operations_account_id
+}
+
+output "delegated_administrator_account_ids" {
+  description = "AWS account IDs configured as delegated administrators for centralized security services"
+
+  value = {
+    securityhub = try(
+      aws_securityhub_organization_admin_account.security_operations[0].admin_account_id,
+      null
+    )
+
+    guardduty = try(
+      aws_guardduty_organization_admin_account.security_operations[0].admin_account_id,
+      null
+    )
+  }
 }
 
 output "securityhub_trusted_access_enabled" {
