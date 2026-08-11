4dc64d8ff37fcbc359c8f793fbe6d4d313a9f8c8
Jacob Molland <jacobmolland28@gmail.com>
2026-08-11 01:21:57 -0400

    fix: remediate more json errors

--- scripts/validation/export-baseline.sh
+++ scripts/validation/export-baseline.sh
@@ -219,7 +219,7 @@ jq -n \
       },
       {
         "validation": "end_user_sso_login_validation",
-        "workflow: null
+        "workflow": null
       },
       {
         "validation": "live_ec2_isolation",
