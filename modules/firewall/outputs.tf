64c80fc50ee49a6b50f81e82ec0d1651610542eb
Jacob Molland <jacobmolland28@gmail.com>
2026-08-19 23:16:32 -0400

    refactor: change output name to 'effective_allowed_egress_domain' from the 'firewall' module

--- modules/firewall/outputs.tf
+++ modules/firewall/outputs.tf
@@ -22,7 +22,7 @@ output "firewall_endpoint_ids_by_az" {
   }
 }
 
-output "allowed_egress_domains" {
-  description = "Domain targets configured on the Network Firewall stateful domain allowlist."
-  value       = aws_networkfirewall_rule_group.stateful_domains.rule_group[0].rules_source[0].rules_source_list[0].targets
+output "effective_allowed_egress_domains" {
+  description = "Effective domain targets configured on the Network Firewall stateful domain allowlist."
+  value = aws_networkfirewall_rule_group.stateful_domains.rule_group[0].rules_source[0].rules_source_list[0].targets
 }
