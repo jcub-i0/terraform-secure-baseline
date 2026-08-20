ab7402b642b75a1aa36404c2488cfc62d18d6c19
Jacob Molland <jacobmolland28@gmail.com>
2026-08-19 22:37:55 -0400

    chore: define 'allowed_egress_domains' output var from 'firwall' module

--- modules/firewall/outputs.tf
+++ modules/firewall/outputs.tf
@@ -21,3 +21,8 @@ output "firewall_endpoint_ids_by_az" {
     s.availability_zone => s.attachment[0].endpoint_id
   }
 }
+
+output "allowed_egress_domains" {
+  description = "Domain targets configured on the Network Firewall stateful domain allowlist."
+  value = aws_networkfirewall_rule_group.stateful_domains.rule_group[0].rules_source[0].rules_source_list[0].targets
+}
