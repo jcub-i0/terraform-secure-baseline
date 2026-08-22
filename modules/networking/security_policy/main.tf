de49d6a25c0ed5fd8d5a6f8c214b91100292fe4a
Jacob Molland <jacobmolland28@gmail.com>
2026-08-21 22:12:53 -0400

    fix: reference var.db_port instead of 443

--- modules/networking/security_policy/main.tf
+++ modules/networking/security_policy/main.tf
@@ -189,8 +189,8 @@ resource "aws_security_group_rule" "ecs_tasks_egress_to_db" {
 
   type                     = "egress"
   security_group_id        = each.value.task_sg_id
-  from_port                = 443
-  to_port                  = 443
+  from_port                = var.db_port
+  to_port                  = var.db_port
   protocol                 = "tcp"
   source_security_group_id = var.data_sg_id
   description              = "ECS tasks to DB"
