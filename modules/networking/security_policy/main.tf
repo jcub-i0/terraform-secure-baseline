c48bd4cae93c824b3cc784f064a88a982df18018
Jacob Molland <jacobmolland28@gmail.com>
2026-08-21 22:01:20 -0400

    chore: define 'endpoints_ingress_from_ecs_tasks' security group rule

--- modules/networking/security_policy/main.tf
+++ modules/networking/security_policy/main.tf
@@ -139,3 +139,15 @@ resource "aws_security_group_rule" "ecs_tasks_egress_to_endpoints" {
   source_security_group_id = var.interface_endpoints_sg_id
   description              = "ECS tasks to Interface VPC Endpoints over HTTPS"
 }
+
+resource "aws_security_group_rule" "endpoints_ingress_from_ecs_tasks" {
+  for_each = var.ecs_security_policy_services
+
+  type = "ingress"
+  security_group_id = var.interface_endpoints_sg_id
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  source_security_group_id = each.value.task_sg_id
+  description = "ECS tasks to Interface VPC Endpoints over HTTPS"
+}
