a3b642edd823318cf57aad13e7e9fc49f9347713
Jacob Molland <jacobmolland28@gmail.com>
2026-08-21 20:10:33 -0400

    chore: define 'ecs_task_execution_role' and 'ecs_task_roles' IAM roles

--- modules/iam/ecs.tf
+++ modules/iam/ecs.tf
@@ -24,3 +24,29 @@ data "aws_iam_policy_document" "ecs_tasks_assume_role" {
     }
   }
 }
+
+resource "aws_iam_role" "ecs_task_execution_roles" {
+  for_each = var.ecs_iam_services
+
+  name = "${var.name_prefix}-${each.key}-ecs-execution"
+  assume_role_policy = data.aws_iam_policy_document.ecs_tasks_assume_role.json
+
+  tags = {
+    Name = "${var.name_prefix}-${each.key}-ecs-execution"
+    Environment = var.environment
+    Terraform = "true"
+  }
+}
+
+resource "aws_iam_role" "ecs_task_roles" {
+  for_each = var.ecs_iam_services
+
+  name = "${var.name_prefix}-${each.key}-ecs-task"
+  assume_role_policy = data.aws_iam_policy_document.ecs_tasks_assume_role.json
+
+  tags = {
+    Name = "${var.name_prefix}-${each.key}-ecs-task"
+    Environment = var.environment
+    Terraform = "true"
+  }
+}
