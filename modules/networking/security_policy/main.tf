7bb4fa149a563e6b91ed301acdae5582bae369d3
Jacob Molland <jacobmolland28@gmail.com>
2026-08-21 21:59:40 -0400

    chore: add 'ecs_tasks_egress_to_endpoints' security group rule to 'security_policy' module

--- modules/networking/security_policy/main.tf
+++ modules/networking/security_policy/main.tf
@@ -126,3 +126,16 @@ resource "aws_security_group_rule" "lambda_rollback_egress_to_endpoints" {
   source_security_group_id = var.interface_endpoints_sg_id
   description              = "Lambda EC2 Rollback to VPC Endpoints over HTTPS"
 }
+
+## ECS TASK SG RULES
+resource "aws_security_group_rule" "ecs_tasks_egress_to_endpoints" {
+  for_each = var.ecs_security_policy_services
+
+  type = "egress"
+  security_group_id = each.value.task_sg_id
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  source_security_group_id = var.interface_endpoints_sg_id
+  description = "ECS tasks to Interface VPC Endpoints over HTTPS"
+}
