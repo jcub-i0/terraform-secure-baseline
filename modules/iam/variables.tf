5fccced141dd353011199272345755cdc7a5bd07
Jacob Molland <jacobmolland28@gmail.com>
2026-08-21 20:05:59 -0400

    chore: define 'ecs_iam_services' in the iam module

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -73,3 +73,18 @@ variable "lambda_ec2_rollback_dlq_arn" {
 variable "lambda_ip_enrichment_dlq_arn" {
   type = string
 }
+
+variable "ecs_iam_services" {
+  description = "ECS services requiring task execution and task IAM roles"
+
+  type = map(object({
+    ecr_repository_arns = set(string)
+    log_group_arns = set(string)
+
+    execution_secret_arns = optional(set(string), [])
+    execution_ssm_parameter_arms = optional(set(string), [])
+    execution_kms_key_arns = optional(set(string), [])
+  }))
+
+  default = {}
+}
