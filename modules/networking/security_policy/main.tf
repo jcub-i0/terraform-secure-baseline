416ad755d790245f0d56d91bb9f05200678779bb
Jacob Molland <jacobmolland28@gmail.com>
2026-08-21 22:12:26 -0400

    style: terraform fmt

--- modules/networking/security_policy/main.tf
+++ modules/networking/security_policy/main.tf
@@ -187,11 +187,11 @@ resource "aws_security_group_rule" "ecs_tasks_egress_to_db" {
     if service.database_access
   }
 
-  type = "egress"
-  security_group_id = each.value.task_sg_id
-  from_port = 443
-  to_port = 443
-  protocol = "tcp"
+  type                     = "egress"
+  security_group_id        = each.value.task_sg_id
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
   source_security_group_id = var.data_sg_id
-  description = "ECS tasks to DB"
+  description              = "ECS tasks to DB"
 }
