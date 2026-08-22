6de74e93cbe57ebceebd072eaad4079207e01f67
Jacob Molland <jacobmolland28@gmail.com>
2026-08-21 23:40:53 -0400

    chore: define 'ecr_repositories' output var form each of the (3) environment workload stacks

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -137,3 +137,8 @@ output "effective_manage_guardduty_locally" {
   description = "Whether GuardDuty resources are managed locally by Terraform in this workload account"
   value       = module.baseline.effective_manage_guardduty_locally
 }
+
+output "ecr_repositories" {
+  description = "Managed ECR repository metadata keyed by repository name"
+  value = module.ecr.repositories
+}
