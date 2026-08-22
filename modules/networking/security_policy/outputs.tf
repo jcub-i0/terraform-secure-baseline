698a0307a9816de6ee3a2ee61b1da8accb446811
Jacob Molland <jacobmolland28@gmail.com>
2026-08-21 22:24:53 -0400

    chore: define 'ecs_sg_rule_ids' output var from the 'security_policy' module

--- modules/networking/security_policy/outputs.tf
+++ modules/networking/security_policy/outputs.tf
@@ -12,3 +12,40 @@ output "compute_sg_rule_ids" {
     )
   }
 }
+
+output "ecs_sg_rule_ids" {
+  description = "Security Group rule IDs that must exist before ECS tasks launch."
+
+  value = {
+    for service_name, service in var.ecs_security_policy_services : service_name => {
+      endpoints_ingress = aws_security_group_rule.endpoints_ingress_from_ecs_tasks[service_name].id
+      endpoints_egress  = aws_security_group_rule.ecs_tasks_egress_to_endpoints[service_name].id
+      s3_egress         = aws_security_group_rule.ecs_tasks_egress_to_s3[service_name].id
+
+      internet_https_egress = try(
+        aws_security_group_rule.ecs_tasks_egress_to_internet_https[service_name].id,
+        null
+      )
+
+      db_egress = try(
+        aws_security_group_rule.ecs_tasks_egress_to_db[service_name].id,
+        null
+      )
+
+      db_ingress = try(
+        aws_security_group_rule.db_ingress_from_ecs_tasks[service_name].id,
+        null
+      )
+
+      alb_ingress = try(
+        aws_security_group_rule.ecs_tasks_ingress_from_alb[service_name].id,
+        null
+      )
+
+      alb_egress = try(
+        aws_security_group_rule.alb_egress_to_ecs_tasks[service_name].id,
+        null
+      )
+    }
+  }
+}
