2e0502ef1906068ce4f7b8db4aab297795425604
Jacob Molland <jacobmolland28@gmail.com>
2026-08-21 23:20:39 -0400

    chore: define 's3_prefix_list_id' output var from the 'vpc_endpoints' module

--- modules/vpc_endpoints/outputs.tf
+++ modules/vpc_endpoints/outputs.tf
@@ -8,3 +8,8 @@ output "interface_endpoint_ids" {
     service => endpoint.id
   }
 }
+
+output "s3_prefix_list_id" {
+  description = "Prefix list ID associated with the S3 Gateway VPC Endpoint"
+  value = aws_vpc_endpoint.s3.prefix_list_id
+}
