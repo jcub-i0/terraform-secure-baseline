01517d9ddbe49d46146833ce8d05caa355c2a04e
Jacob Molland <jacobmolland28@gmail.com>
2026-08-24 23:40:12 -0400

    style: terraform fmt

--- modules/application_load_balancer/main.tf
+++ modules/application_load_balancer/main.tf
@@ -69,28 +69,28 @@ resource "aws_lb_listener" "https" {
 resource "aws_lb_target_group" "target_groups" {
   for_each = var.services
 
-  name = "${var.name_prefix}-${each.key}"
-  port = each.value.container_port
-  protocol = "HTTP"
-  vpc_id = var.vpc_id
+  name        = "${var.name_prefix}-${each.key}"
+  port        = each.value.container_port
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
   target_type = "ip"
 
   health_check {
-    enabled = true
+    enabled  = true
     protocol = "HTTP"
-    path = each.value.health_check_path
-    matcher = "200-399"
+    path     = each.value.health_check_path
+    matcher  = "200-399"
   }
 
   tags = {
-    Name = "${var.name_prefix}-${each.key}"
+    Name        = "${var.name_prefix}-${each.key}"
     Environment = var.environment
-    Terraform = "true"
+    Terraform   = "true"
   }
 
   lifecycle {
     precondition {
-      condition = length("${var.name_prefix}-${each.key}") <= 32
+      condition     = length("${var.name_prefix}-${each.key}") <= 32
       error_message = "The complete target group name must not exceed 32 characters."
     }
   }
