14e42e5a19763213e499991aa172823713a7ebf6
Jacob Molland <jacobmolland28@gmail.com>
2026-08-24 21:48:49 -0400

    style: terraform fmt

--- modules/ecs_cluster/outputs.tf
+++ modules/ecs_cluster/outputs.tf
@@ -1,9 +1,9 @@
 output "cluster_arn" {
   description = "ARN of the ECS cluster"
-  value = aws_ecs_cluster.cluster.arn
+  value       = aws_ecs_cluster.cluster.arn
 }
 
 output "cluster_name" {
   description = "Name of the ECS cluster"
-  value = aws_ecs_cluster.cluster.name
+  value       = aws_ecs_cluster.cluster.name
 }
