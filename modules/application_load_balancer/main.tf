f7e251bb0c94d7cbbbe243e26ad11f297aa50475
Jacob Molland <jacobmolland28@gmail.com>
2026-08-24 23:39:45 -0400

    feat: create 'target_groups' loudbalancer target group resource

--- modules/application_load_balancer/main.tf
+++ modules/application_load_balancer/main.tf
@@ -65,3 +65,33 @@ resource "aws_lb_listener" "https" {
     }
   }
 }
+
+resource "aws_lb_target_group" "target_groups" {
+  for_each = var.services
+
+  name = "${var.name_prefix}-${each.key}"
+  port = each.value.container_port
+  protocol = "HTTP"
+  vpc_id = var.vpc_id
+  target_type = "ip"
+
+  health_check {
+    enabled = true
+    protocol = "HTTP"
+    path = each.value.health_check_path
+    matcher = "200-399"
+  }
+
+  tags = {
+    Name = "${var.name_prefix}-${each.key}"
+    Environment = var.environment
+    Terraform = "true"
+  }
+
+  lifecycle {
+    precondition {
+      condition = length("${var.name_prefix}-${each.key}") <= 32
+      error_message = "The complete target group name must not exceed 32 characters."
+    }
+  }
+}
