9d48339f18babcb14b8930adb25b02ed8f22494a
Jacob Molland <jacobmolland28@gmail.com>
2026-08-24 22:36:49 -0400

    style: terraform fmt

--- modules/application_load_balancer/main.tf
+++ modules/application_load_balancer/main.tf
@@ -1,11 +1,11 @@
 resource "aws_security_group" "load_balancer" {
-  name = "${var.name_prefix}-ALB-SG"
+  name        = "${var.name_prefix}-ALB-SG"
   description = "Security Group for the Application Load Balancer"
-  vpc_id = var.vpc_id
+  vpc_id      = var.vpc_id
 
   tags = {
-    Name = "${var.name_prefix}-ALB-SG"
+    Name        = "${var.name_prefix}-ALB-SG"
     Environment = var.environment
-    Terraform = "true"
+    Terraform   = "true"
   }
 }
