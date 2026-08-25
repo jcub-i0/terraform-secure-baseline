72670cc8008035aa57fb0876c342bbb9761e8f86
Jacob Molland <jacobmolland28@gmail.com>
2026-08-24 22:38:15 -0400

    feat: create 'https_ingress' SG rule for the load_balancer SG

--- modules/application_load_balancer/main.tf
+++ modules/application_load_balancer/main.tf
@@ -9,3 +9,13 @@ resource "aws_security_group" "load_balancer" {
     Terraform   = "true"
   }
 }
+
+resource "aws_security_group_rule" "https_ingress" {
+  type = "ingress"
+  security_group_id = aws_security_group.load_balancer.id
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  cidr_blocks = var.ingress_cidrs
+  description = "Allow HTTPS ingress to the Application Load Balancer"
+}
