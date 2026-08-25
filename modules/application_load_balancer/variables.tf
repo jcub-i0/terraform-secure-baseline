a1f8de66b3e1ffc1964ea955f497606e22ef080b
Jacob Molland <jacobmolland28@gmail.com>
2026-08-24 22:18:21 -0400

    chore: define 'public_subnet_ids', 'certificate_arn', 'ingress_cidrs', and 'ssl_policy' variables in the application_load_balancer module

--- modules/application_load_balancer/variables.tf
+++ modules/application_load_balancer/variables.tf
@@ -11,3 +11,24 @@ variable "environment" {
 variable "vpc_id" {
   type = string
 }
+
+variable "public_subnet_ids" {
+  description = "Public subnet IDs used by the Internet-facing Application Load Balancer"
+  type = set(string)
+}
+
+variable "certificate_arn" {
+  description = "ACM certificate ARN used by the HTTPS listener"
+  type = string
+}
+
+variable "ingress_cidrs" {
+  description = "IPv4 CIDR blocks allowed to reach teh HTTPS listener"
+  type = set(string)
+}
+
+variable "ssl_policy" {
+  description = "TLS security policy used by the HTTPS listener"
+  type = string
+  default = "ELBSecurityPolicy-TLS13-1-2-Res-PQ-2025-09"
+}
