0f3bc029b11ef5b608a7e349a7152c9988dffab1
Jacob Molland <jacobmolland28@gmail.com>
2026-08-24 21:47:17 -0400

    feat: define 'cluster' aws_ecs_cluster resource

--- modules/ecs_cluster/main.tf
+++ modules/ecs_cluster/main.tf
@@ -1,3 +1,17 @@
 #####################
 # ECS_CLUSTER MODULE
 #####################
+resource "aws_ecs_cluster" "cluster" {
+  name = "${var.name_prefix}-ecs"
+
+  setting {
+    name = "containerInsights"
+    value = var.container_insights
+  }
+
+  tags = {
+    Name = "${var.name_prefix}-ecs"
+    Environment = var.environment
+    Terraform = "true"
+  }
+}
