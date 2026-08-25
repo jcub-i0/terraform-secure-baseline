3409cd670c465933f646458de3897f4fd82d84b4
Jacob Molland <jacobmolland28@gmail.com>
2026-08-24 22:18:39 -0400

    style: terraform fmt

--- modules/application_load_balancer/variables.tf
+++ modules/application_load_balancer/variables.tf
@@ -14,21 +14,21 @@ variable "vpc_id" {
 
 variable "public_subnet_ids" {
   description = "Public subnet IDs used by the Internet-facing Application Load Balancer"
-  type = set(string)
+  type        = set(string)
 }
 
 variable "certificate_arn" {
   description = "ACM certificate ARN used by the HTTPS listener"
-  type = string
+  type        = string
 }
 
 variable "ingress_cidrs" {
   description = "IPv4 CIDR blocks allowed to reach teh HTTPS listener"
-  type = set(string)
+  type        = set(string)
 }
 
 variable "ssl_policy" {
   description = "TLS security policy used by the HTTPS listener"
-  type = string
-  default = "ELBSecurityPolicy-TLS13-1-2-Res-PQ-2025-09"
+  type        = string
+  default     = "ELBSecurityPolicy-TLS13-1-2-Res-PQ-2025-09"
 }
