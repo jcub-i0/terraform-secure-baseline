cdfc0c8bba6ab782a9767c37b4f98a9523d78127
Jacob Molland <jacobmolland28@gmail.com>
2026-08-24 22:36:34 -0400

    feat: create load_balancer SG

---
+++ modules/application_load_balancer/main.tf
@@ -0,0 +1,11 @@
+resource "aws_security_group" "load_balancer" {
+  name = "${var.name_prefix}-ALB-SG"
+  description = "Security Group for the Application Load Balancer"
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = "${var.name_prefix}-ALB-SG"
+    Environment = var.environment
+    Terraform = "true"
+  }
+}
