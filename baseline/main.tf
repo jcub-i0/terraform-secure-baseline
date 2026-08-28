f55d27503b9a714a3fa832f4234b84fe4ac2fdd4
Jacob Molland <jacobmolland28@gmail.com>
2026-08-28 16:20:07 -0400

    chore: pass the 'ecs_iam_services' local var into the 'iam' module

--- baseline/main.tf
+++ baseline/main.tf
@@ -118,6 +118,8 @@ module "iam" {
   lambda_ec2_isolation_dlq_arn = module.automation.lambda_ec2_isolation_dlq_arn
   lambda_ec2_rollback_dlq_arn  = module.automation.lambda_ec2_rollback_dlq_arn
   lambda_ip_enrichment_dlq_arn = module.automation.lambda_ip_enrichment_dlq_arn
+
+  ecs_iam_services = local.ecs_iam_services
 }
 
 module "security" {
