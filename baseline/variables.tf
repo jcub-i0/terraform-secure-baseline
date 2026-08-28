7cabb013d5c4546621a2f50e41c3e2290432b474
Jacob Molland <jacobmolland28@gmail.com>
2026-08-28 16:36:12 -0400

    chore: add validation to 'ecs_services' variable in the 'baseline' stack to prevent a plaintext environment variable from colliding with a secret variable

--- baseline/variables.tf
+++ baseline/variables.tf
@@ -467,4 +467,19 @@ variable "ecs_services" {
 
     error_message = "An ECS service secret name cannot be defined in both secrets_manager_secrets and ssm_parameters."
   }
+
+  validation {
+    condition = alltrue([
+      for service in values(var.ecs_services) :
+      length(setintersection(
+        toset(keys(service.environment_variables)),
+        setunion(
+          toset(keys(service.secrets_manager_secrets)),
+          toset(keys(service.ssm_parameters)),
+        ),
+      )) == 0
+    ])
+
+    error_message = "An ECS service environment variable name cannot also be defined as a Secrets Manager secret or SSM parameter."
+  }
 }
