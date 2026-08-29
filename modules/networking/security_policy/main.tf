8f899b58252e56d42e8d280a3002b84a2916e810
Jacob Molland <jacobmolland28@gmail.com>
2026-08-28 23:43:00 -0400

    fix: reference ecs_security_policy_services.alb_access

--- modules/networking/security_policy/main.tf
+++ modules/networking/security_policy/main.tf
@@ -216,7 +216,7 @@ resource "aws_security_group_rule" "ecs_tasks_ingress_from_alb" {
   for_each = {
     for service_name, service in var.ecs_security_policy_services :
     service_name => service
-    if service.alb_sg_id != null
+    if service.alb_access
   }
 
   type                     = "ingress"
@@ -232,7 +232,7 @@ resource "aws_security_group_rule" "alb_egress_to_ecs_tasks" {
   for_each = {
     for service_name, service in var.ecs_security_policy_services :
     service_name => service
-    if service.alb_sg_id != null
+    if service.alb_access
   }
 
   type                     = "egress"
