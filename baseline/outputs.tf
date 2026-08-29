f09fa53d84b52fbab80ae0d62af59f42d938576c
Jacob Molland <jacobmolland28@gmail.com>
2026-08-29 19:03:53 -0400

    chore: define 'logs_cmk_arn' output from the 'baseline' stack

--- baseline/outputs.tf
+++ baseline/outputs.tf
@@ -58,6 +58,11 @@ output "secrets_manager_cmk_arn" {
   value       = module.security.secrets_manager_cmk_arn
 }
 
+output "logs_cmk_arn" {
+  description = "ARN of the CMK used to encrypt workload CloudWatch Logs"
+  value = module.security.logs_cmk_arn
+}
+
 output "logs_cmk_decrypt_policy_name" {
   description = "'Name' attribute of the 'Logs CMK Decrypt Policy' resource"
   value       = module.iam.logs_cmk_decrypt_policy_name
