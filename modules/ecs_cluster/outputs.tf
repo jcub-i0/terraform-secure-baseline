e2faf1883558bf77110d6c9ec1c440d7eb3b51d1
Jacob Molland <jacobmolland28@gmail.com>
2026-08-29 20:24:12 -0400

    chore: define 'container_insights' variable from the 'ecs_cluster' module

--- modules/ecs_cluster/outputs.tf
+++ modules/ecs_cluster/outputs.tf
@@ -7,3 +7,12 @@ output "cluster_name" {
   description = "Name of the ECS cluster"
   value       = aws_ecs_cluster.cluster.name
 }
+
+output "container_insights" {
+  description = "Configured ECS Container Insights setting"
+  value = one([
+    for setting in aws_ecs_cluster.cluster.setting :
+    setting.value
+    if setting.name == "containerInsights"
+  ])
+}
