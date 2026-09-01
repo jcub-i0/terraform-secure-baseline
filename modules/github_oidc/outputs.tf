41d4fac4667de207627c23ea2431868bffa92cbc
Jacob Molland <jacobmolland28@gmail.com>
2026-08-31 23:07:24 -0400

    chore: define 'image_publisher_github_arn' output from the 'github_oidc' module

--- modules/github_oidc/outputs.tf
+++ modules/github_oidc/outputs.tf
@@ -7,3 +7,12 @@ output "apply_role_github_arn" {
   description = "ARN of the GitHub OIDC Terraform plan role"
   value       = var.enable_apply_role_github ? aws_iam_role.github_apply[0].arn : null
 }
+
+output "image_publisher_github_arn" {
+  description = "ARN of the GitHub OIDC image publisher role"
+  value = (
+    var.enable_image_publisher_role_github
+    ? aws_iam_role.github_image_publisher[0].arn
+    : null
+  )
+}
