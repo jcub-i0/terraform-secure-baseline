e0570cd1ed7fc55ed030db0fa6ed3463c5a0c352
Jacob Molland <jacobmolland28@gmail.com>
2026-08-31 23:37:53 -0400

    fix: change '.arn' attribute to '.name' in aws_iam_role_policy_attachment.github_image_publisher_attachment.role

--- modules/github_oidc/main.tf
+++ modules/github_oidc/main.tf
@@ -386,6 +386,6 @@ resource "aws_iam_role" "github_image_publisher" {
 resource "aws_iam_role_policy_attachment" "github_image_publisher_attachment" {
   count = var.enable_image_publisher_role_github ? 1 : 0
 
-  role       = aws_iam_role.github_image_publisher[0].arn
+  role       = aws_iam_role.github_image_publisher[0].name
   policy_arn = aws_iam_policy.github_image_publisher[0].arn
 }
