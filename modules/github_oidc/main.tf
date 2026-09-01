dfcaf6f7c1cf615e6815597e581e988d30401dd0
Jacob Molland <jacobmolland28@gmail.com>
2026-08-31 23:20:49 -0400

    fix: typo

--- modules/github_oidc/main.tf
+++ modules/github_oidc/main.tf
@@ -373,7 +373,7 @@ resource "aws_iam_role" "github_image_publisher" {
   count = var.enable_image_publisher_role_github ? 1 : 0
 
   name               = "${var.name_prefix}-github-image-publisher-role"
-  assume_role_policy = data.aws_iam_policy_document.image_publisher_oidc_assume_rolej[0].json
+  assume_role_policy = data.aws_iam_policy_document.image_publisher_oidc_assume_role[0].json
 }
 
 resource "aws_iam_role_policy_attachment" "github_image_publisher_attachment" {
