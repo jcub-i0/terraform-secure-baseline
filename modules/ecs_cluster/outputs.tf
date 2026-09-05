5e3ad890619922d1a7308d87966ed945f038680a
Jacob Molland <jacobmolland28@gmail.com>
2026-09-04 22:15:01 -0400

    chore: add 'container_insights_log_group' output var from ecs_cluster module

--- modules/ecs_cluster/outputs.tf
+++ modules/ecs_cluster/outputs.tf
@@ -16,3 +16,14 @@ output "container_insights" {
     if setting.name == "containerInsights"
   ])
 }
+
+output "container_insights_log_group" {
+  description = "Terraform-managed Container Insights performance log-group metadata; null when Container Insights is disabled"
+
+  value = var.container_insights == "disabled" ? null : {
+    arn               = aws_cloudwatch_log_group.container_insights_performance[0].arn
+    name              = aws_cloudwatch_log_group.container_insights_performance[0].name
+    retention_in_days = aws_cloudwatch_log_group.container_insights_performance[0].retention_in_days
+    kms_key_id        = aws_cloudwatch_log_group.container_insights_performance[0].kms_key_id
+  }
+}
