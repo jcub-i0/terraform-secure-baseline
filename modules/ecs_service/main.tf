e5a7c29d10df059a03afddadeb13905d42f6e937
Jacob Molland <jacobmolland28@gmail.com>
2026-09-10 00:25:42 -0400

    style: terraform fmt

--- modules/ecs_service/main.tf
+++ modules/ecs_service/main.tf
@@ -257,17 +257,17 @@ resource "aws_appautoscaling_target" "ecs_services" {
 resource "aws_appautoscaling_policy" "ecs_cpu_target_tracking" {
   for_each = local.cpu_target_tracking_services
 
-  name = "${var.name_prefix}-${each.key}-ecs-cpu-target-tracking"
+  name        = "${var.name_prefix}-${each.key}-ecs-cpu-target-tracking"
   policy_type = "TargetTrackingScaling"
 
-  resource_id = aws_appautoscaling_target.ecs_services[each.key].resource_id
+  resource_id        = aws_appautoscaling_target.ecs_services[each.key].resource_id
   scalable_dimension = aws_appautoscaling_target.ecs_services[each.key].scalable_dimension
-  service_namespace = aws_appautoscaling_target.ecs_services[each.key].service_namespace
+  service_namespace  = aws_appautoscaling_target.ecs_services[each.key].service_namespace
 
   target_tracking_scaling_policy_configuration {
     target_value = each.value.scaling.cpu_target_permit
 
-    scale_in_cooldown = each.value.scaling.scale_in_cooldown_seconds
+    scale_in_cooldown  = each.value.scaling.scale_in_cooldown_seconds
     scale_out_cooldown = each.value.scaling.scale_out_cooldown_seconds
 
     predefined_metric_specification {
