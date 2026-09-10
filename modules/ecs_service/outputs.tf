36d8575a393455d9a76444e544b3d61b95558468
Jacob Molland <jacobmolland28@gmail.com>
2026-09-09 23:00:33 -0400

    chore: update 'services' output variable from the ecs_service module to include BOTH aws_ecs_service.services and aws_ecs_service.autoscaled_services

--- modules/ecs_service/outputs.tf
+++ modules/ecs_service/outputs.tf
@@ -31,12 +31,22 @@ output "task_definition_arns" {
 output "services" {
   description = "ECS service metadata keyed by service name"
 
-  value = {
-    for service_name, service in aws_ecs_service.services :
-    service_name => {
-      arn              = service.arn
-      name             = service.name
-      platform_version = service.platform_version
-    }
-  }
+  value = merge(
+    {
+      for service_name, service in aws_ecs_service.services :
+      service_name => {
+        arn              = service.arn
+        name             = service.name
+        platform_version = service.platform_version
+      }
+    },
+    {
+      for service_name, service in aws_ecs_service.autoscaled_services :
+      service_name => {
+        arn = service.arn
+        name = service.name
+        platform_version = service.platform_version
+      }
+    },
+  )
 }
