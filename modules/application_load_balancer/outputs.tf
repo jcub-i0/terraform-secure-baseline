7e3be003bb57e4cb7b5ef9aeeff23b5551330045
Jacob Molland <jacobmolland28@gmail.com>
2026-09-11 15:39:21 -0400

    chore: update logic in how target_groups output resolves its values

--- modules/application_load_balancer/outputs.tf
+++ modules/application_load_balancer/outputs.tf
@@ -33,8 +33,9 @@ output "target_groups" {
 
   value = {
     for service_name, target_group in aws_lb_target_group.target_groups : service_name => {
-      arn  = target_group.arn
-      name = target_group.name
+      arn        = target_group.arn
+      arn_suffix = target_group.arn_suffix
+      name       = target_group.name
     }
   }
 }
