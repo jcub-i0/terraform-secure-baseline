b06b1620f7678503e039f89c5fa0706f22ddee0c
Jacob Molland <jacobmolland28@gmail.com>
2026-09-11 21:27:38 -0400

    chore: define local.ecs_runtime_services.service_name.deployment in 'baseline' stack

--- baseline/locals.tf
+++ baseline/locals.tf
@@ -182,6 +182,7 @@ locals {
       memory         = service.memory
       desired_count  = service.desired_count
       scaling        = service.scaling
+      deployment    = service.deployment
 
       execution_role_arn = module.iam.ecs_task_execution_roles[service_name].arn
       task_role_arn      = module.iam.ecs_task_roles[service_name].arn
