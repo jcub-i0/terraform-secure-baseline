7b164c6c6faadc9f65dce353864f059155929f89
Jacob Molland <jacobmolland28@gmail.com>
2026-09-18 21:38:50 -0400

    refactor: remove unused 'backup_tag_value' var in the 'backup' module

--- modules/backup/variables.tf
+++ modules/backup/variables.tf
@@ -33,9 +33,3 @@ variable "backup_tag_key" {
   type        = string
   default     = "Backup"
 }
-
-variable "backup_tag_value" {
-  description = "Tag value used to select resources for backup"
-  type        = string
-  default     = "true"
-}
